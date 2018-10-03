f502e3e3069085827993c2517d5931280a5355bb
akrock <andrew.krock@gmail.com>
2018-10-03 13:34:56 -0400

    Fix for #69
    Handle the OverflowExcpetion and return -1.

--- Snowflake.Data/Core/ResultSetUtil.cs
+++ Snowflake.Data/Core/ResultSetUtil.cs
@@ -17,32 +17,57 @@ namespace Snowflake.Data.Core
             SFResultSetMetaData metaData = resultSet.sfResultSetMetaData;
             SFStatementType statementType = metaData.statementType;
 
-            int updateCount = 0;
-            switch (statementType)
+            try
             {
-                case SFStatementType.INSERT:
-                case SFStatementType.UPDATE:
-                case SFStatementType.DELETE:
-                case SFStatementType.MERGE:
-                case SFStatementType.MULTI_INSERT:
-                    for (int i = 0; i < resultSet.columnCount; i++)
-                    {
-                        updateCount += resultSet.GetValue<int>(i);
-                    }
-                    break;
-                case SFStatementType.COPY:
-                    var index = resultSet.sfResultSetMetaData.getColumnIndexByName("rows_loaded");
-                    if (index >= 0) updateCount = resultSet.GetValue<int>(index);
-                    break;
-                case SFStatementType.SELECT:
-                    updateCount = -1;
-                    break;
-                default:
-                    updateCount = 0;
-                    break;
-            }
+                int updateCount = 0;
+                switch (statementType)
+                {
+                    case SFStatementType.INSERT:
+                    case SFStatementType.UPDATE:
+                    case SFStatementType.DELETE:
+                    case SFStatementType.MERGE:
+                    case SFStatementType.MULTI_INSERT:
+                        for (int i = 0; i < resultSet.columnCount; i++)
+                        {
+                            updateCount += resultSet.GetValue<int>(i);
+                        }
 
-            return updateCount;
+                        break;
+                    case SFStatementType.COPY:
+                        var index = resultSet.sfResultSetMetaData.getColumnIndexByName("rows_loaded");
+                        if (index >= 0) updateCount = resultSet.GetValue<int>(index);
+                        break;
+                    case SFStatementType.SELECT:
+                        updateCount = -1;
+                        break;
+                    default:
+                        updateCount = 0;
+                        break;
+                }
+
+                return updateCount;
+            }
+            catch (Exception ex)
+            {
+                if (IsOverflowException(ex))
+                    return -1;
+
+                throw;
+            }
+        }
+
+        private static bool IsOverflowException(Exception ex)
+        {
+            if (ex is OverflowException)
+                return true;
+
+            if (ex.InnerException != null)
+                return IsOverflowException(ex.InnerException);
+
+            if (ex is AggregateException aggEx)
+                return aggEx.InnerExceptions.Any(IsOverflowException);
+
+            return false;
         }
     }
 }
