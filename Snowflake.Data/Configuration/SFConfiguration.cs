2324e846b229fc6e02dce8a4dd1e93dc0ba81018
Shige Takeda <smtakeda@gmail.com>
2019-04-22 09:33:41 -0700

    SNOW-74563: Added a chunkdownloader switch to back to old algorithm

--- Snowflake.Data/Configuration/SFConfiguration.cs
+++ Snowflake.Data/Configuration/SFConfiguration.cs
@@ -10,12 +10,18 @@ namespace Snowflake.Data.Configuration
     {
         public Boolean UseV2JsonParser;
 
+        // Leave this configuration for backward compatibility.
+        // We would discard it after we announce this change.
+        // Right now, when this is true, it would disable the ChunkDownloaderVersion
         public Boolean UseV2ChunkDownloader;
+
+        public int ChunkDownloaderVersion;
 
         private SFConfiguration()
         {
             UseV2JsonParser = true;
             UseV2ChunkDownloader = false;
+            ChunkDownloaderVersion = 3;
         }
 
         private readonly static SFConfiguration Config = new SFConfiguration();
