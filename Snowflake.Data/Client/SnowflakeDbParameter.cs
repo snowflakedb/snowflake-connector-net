b6419dc5c8ec83569ddd13cbf9b91d771f2835d6
Tianyi Chen <ChTimTsubasa@users.noreply.github.com>
2019-06-21 16:04:57 -0700

    Revert "@snow-81118 Not being strict on DBParameter to unblock potential use case from frameworks such as EntityFrameworkCore (#145)"
    This reverts commit c68780efd8c3a1bd5afea00e64f7d6055b2e1a73.

--- Snowflake.Data/Client/SnowflakeDbParameter.cs
+++ Snowflake.Data/Client/SnowflakeDbParameter.cs
@@ -3,6 +3,10 @@
  */
 
 using System.Data.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System;
 using System.Data;
 using Snowflake.Data.Core;
@@ -13,19 +17,15 @@ namespace Snowflake.Data.Client
     {
         public SFDataType SFDataType { get; set; }
 
-        private SFDataType OriginType;
-
         public SnowflakeDbParameter()
         {
             SFDataType = SFDataType.None;
-            OriginType = SFDataType.None;
         }
 
         public SnowflakeDbParameter(string ParameterName, SFDataType SFDataType)
         {
             this.ParameterName = ParameterName;
             this.SFDataType = SFDataType;
-            OriginType = SFDataType;
         }
 
         public SnowflakeDbParameter(int ParameterIndex, SFDataType SFDataType)
@@ -52,21 +52,75 @@ namespace Snowflake.Data.Client
             }
         }
 
-        public override bool IsNullable { get; set; }
+        public override bool IsNullable
+        {
+            get { return false; }
 
-        public override string ParameterName { get; set; }
+            set
+            {
+                if (value != false)
+                {
+                    throw new SnowflakeDbException(SFError.UNSUPPORTED_FEATURE);
+                }
+            }
+        }
 
-        public override int Size { get; set; }
+        public override string ParameterName
+        {
+            get;
+            set;
+        }
 
-        public override string SourceColumn { get; set; }
+        public override int Size
+        {
+            get { return 0; }
 
-        public override bool SourceColumnNullMapping { get; set; }
+            set
+            {
+                if (value != 0)
+                {
+                    throw new SnowflakeDbException(SFError.UNSUPPORTED_FEATURE);
+                }
+            }
+        }
 
-        public override object Value { get; set; }
+        public override string SourceColumn
+        {
+            get { return null; }
+
+            set
+            {
+                if (value != null)
+                {
+                    throw new SnowflakeDbException(SFError.UNSUPPORTED_FEATURE);
+                }
+            }
+        }
+
+        public override bool SourceColumnNullMapping
+        {
+            get { return false; }
+
+            set
+            {
+                // ReSharper disable once RedundantBoolCompare (compare to false for clarity)
+                if (value != false)
+                {
+                    throw  new SnowflakeDbException(SFError.UNSUPPORTED_FEATURE);
+                }
+            }
+        }
+
+        public override object Value
+        {
+            get;
+
+            set;
+        }
 
         public override void ResetDbType()
         {
-            SFDataType = OriginType;
+            throw new NotImplementedException();
         }
     }
 }
