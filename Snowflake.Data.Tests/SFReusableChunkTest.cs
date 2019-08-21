bec3bb6b1fc19d25dcd16a39af2a50a5b3d7084e
John Fisher <jfisher@peoplenetonline.com>
2019-08-21 12:35:48 -0500

    Detect improper handling of dates in JSON strings

--- Snowflake.Data.Tests/SFReusableChunkTest.cs
+++ Snowflake.Data.Tests/SFReusableChunkTest.cs
@@ -67,5 +67,33 @@ namespace Snowflake.Data.Tests
             Assert.AreEqual(null, chunk.ExtractCell(1, 1));
             Assert.AreEqual("fghi", chunk.ExtractCell(1, 2));
         }
+        
+        [Test]
+        public void TestChunkWithDate()
+        {
+            string data = "[ [null, \"2019-08-21T11:58:00\", null],  [\"2\", null, \"fghi\"] ]";
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+            Stream stream = new MemoryStream(bytes);
+            IChunkParser parser = new ReusableChunkParser(stream);
+
+            ExecResponseChunk chunkInfo = new ExecResponseChunk()
+            {
+                url = "fake",
+                uncompressedSize = 100,
+                rowCount = 2
+            };
+
+            SFReusableChunk chunk = new SFReusableChunk(3);
+            chunk.Reset(chunkInfo, 0);
+
+            parser.ParseChunk(chunk);
+
+            Assert.AreEqual(null, chunk.ExtractCell(0, 0));
+            Assert.AreEqual("2019-08-21T11:58:00", chunk.ExtractCell(0, 1));
+            Assert.AreEqual(null, chunk.ExtractCell(0, 2));
+            Assert.AreEqual("2", chunk.ExtractCell(1, 0));
+            Assert.AreEqual(null, chunk.ExtractCell(1, 1));
+            Assert.AreEqual("fghi", chunk.ExtractCell(1, 2));
+        }
     }
 }
