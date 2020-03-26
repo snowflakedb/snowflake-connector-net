7331e46dea0cc28d59a2660e0ee71d7d9fa4a28a
Nathan Sfard <nathan.sfard@snowflake.com>
2020-03-26 08:45:42 -0700

    making batch file

---
+++ wss.bat
@@ -0,0 +1,20 @@
+REM Run whitesource for components which need versioning
+
+SET SCAN_DIRECTORIES="%cd%"
+
+IF "%PRODUCT_NAME%"=="" SET PRODUCT_NAME="Snowflake_Drivers_Connectors"
+
+SET PROJECT_NAME="Dot-Net-Connector"
+
+SET DATE="%date%"
+
+curl -LJO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
+
+SET WSS_CONFIG="wss-net.config"
+
+java -jar wss-unified-agent.jar -apiKey %WHITESOURCE_API_KEY%^
+   -c %WSS_CONFIG%^
+   -project %PROJECT_NAME%^
+   -product %PRODUCT_NAME%^
+   -projectVersion %DATE%^
+   -d ${SCAN_DIRECTORIES%
