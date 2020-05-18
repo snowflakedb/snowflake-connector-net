84b27fcdf2f477684224ddcf11daecb0e69ee63f
Shige Takeda <shige.takeda@snowflake.com>
2020-05-18 15:52:08 -0700

    SNOW-160662 updated the wss name (#207)

--- wss.bat
+++ wss.bat
@@ -1,41 +1,54 @@
 REM Run whitesource for components which need versioning
+@echo off
+setlocal
 
+if not defined WHITESOURCE_API_KEY (
+    echo == No WHITESOURCE_API_KEY is set. Skipping WhiteSource scan
+    exit /b 0
+)
 SET SCAN_DIRECTORIES="%cd%"
 
-IF "%PRODUCT_NAME%"=="" SET PRODUCT_NAME="Snowflake_Drivers_Connectors"
+SET PRODUCT_NAME=DotNETDriver
+SET PROJECT_NAME=DotNETDriver
 
-SET PROJECT_NAME="Dot-Net-Connector"
-
-SET DATE="%date%"
+REM Format MM-DD-YYYY
+SET CURRENT_DATE=%date:~4,2%-%date:~7,2%-%date:~10,4%
 
 curl -LJO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
 
 SET WSS_CONFIG="wss-net.config"
+COPY %WSS_CONFIG%.templ %WSS_CONFIG%
 
 java -jar wss-unified-agent.jar -apiKey %WHITESOURCE_API_KEY%^
    -c %WSS_CONFIG%^
    -project %PROJECT_NAME%^
    -product %PRODUCT_NAME%^
-   -projectVersion %DATE%^
    -d %SCAN_DIRECTORIES%^
    -wss.url https://saas.whitesourcesoftware.com/agent^
    -offline true
+IF %ERRORLEVEL% NEQ 0 (
+    echo == failed to run WSS for %PRODUCT_NAME%_%PROJECT_NAME% in offline mode
+    exit /b 1
+)
 
 java -jar wss-unified-agent.jar -apiKey %WHITESOURCE_API_KEY%^
     -c %WSS_CONFIG%^
-    -project %PROJECT_NAME%^
     -product %PRODUCT_NAME%^
+    -project %PROJECT_NAME%^
     -projectVersion baseline^
     -requestFiles whitesource\update-request.txt^
     -wss.url https://saas.whitesourcesoftware.com/agent
 
 IF %ERRORLEVEL% NEQ 0 (
-	ECHO "checkPolicies=false" >> %WSS_CONFIG% && java -jar wss-unified-agent.jar -apiKey %WHITESOURCE_API_KEY%^
-	    -c %WSS_CONFIG%^
-	    -project %PROJECT_NAME%^
-	    -product %PRODUCT_NAME%^
-	    -projectVersion %DATE%^
-	    -requestFiles whitesource\update-request.txt^
-	    -wss.url https://saas.whitesourcesoftware.com/agent
+    echo == failed to run WSS for %PRODUCT_NAME%_%PROJECT_NAME% with baseline
+    exit /b 1
 )
+echo checkPolicies=false>>%WSS_CONFIG%
+java -jar wss-unified-agent.jar -apiKey %WHITESOURCE_API_KEY%^
+    -c %WSS_CONFIG%^
+    -product %PRODUCT_NAME%^
+    -project %PROJECT_NAME%^
+    -projectVersion %CURRENT_DATE%^
+    -requestFiles whitesource\update-request.txt^
+    -wss.url https://saas.whitesourcesoftware.com/agent
 
