3e940bd000c21ce79ea7f651548f42ff48669b01
SIMBA\natachab <natachab>
2021-01-28 15:29:10 -0800

    Adding support for private key content passed directly in the connection string. Allow PKCS1 and PKCS8 format.

--- Snowflake.Data/Core/Authenticator/IAuthenticator.cs
+++ Snowflake.Data/Core/Authenticator/IAuthenticator.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2012-2019 Snowflake Computing Inc. All rights reserved.
+ * Copyright (c) 2012-2021 Snowflake Computing Inc. All rights reserved.
  */
 
 using System;
@@ -146,11 +146,12 @@ namespace Snowflake.Data.Core.Authenticator
             else if (type.Equals(KeyPairAuthenticator.AUTH_NAME, StringComparison.InvariantCultureIgnoreCase))
             {
                 // Get private key path or private key from connection settings
-                if (!session.properties.TryGetValue(SFSessionProperty.PRIVATE_KEY_FILE, out var pkPath))
+                if (!session.properties.TryGetValue(SFSessionProperty.PRIVATE_KEY_FILE, out var pkPath) &&
+                    !session.properties.TryGetValue(SFSessionProperty.PRIVATE_KEY, out var pkContent))
                 {
                     // There is no PRIVATE_KEY_FILE defined, can't authenticate with key-pair
                     string invalidStringDetail =
-                        "Missing required PRIVATE_KEY_FILE for key pair authentication";
+                        "Missing required PRIVATE_KEY_FILE or PRIVATE_KEY for key pair authentication";
                     var error = new SnowflakeDbException(
                         SFError.INVALID_CONNECTION_STRING,
                         new object[] { invalidStringDetail });
