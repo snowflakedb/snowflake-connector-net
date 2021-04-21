a155d8e9dbd4344ac3bd257182474ef7ff205bc5
David Nawn <david.nawn@olo.com>
2021-04-21 09:09:50 -0400

    Trap any exceptions thrown by Close when disposing of SnowflakeDbConnection

--- Snowflake.Data/Client/SnowflakeDbConnection.cs
+++ Snowflake.Data/Client/SnowflakeDbConnection.cs
@@ -161,7 +161,16 @@ namespace Snowflake.Data.Client
             if (disposed)
                 return;
 
-            this.Close();
+            try
+            {
+                this.Close();
+            } 
+            catch (Exception ex)
+            {
+                // Prevent an exception from being thrown when disposing of this object
+                logger.Error("Unable to close connection", ex);
+            }
+            
             disposed = true;
 
             base.Dispose(disposing);
