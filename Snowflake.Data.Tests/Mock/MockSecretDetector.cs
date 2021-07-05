e6ebae1ed5e01d0dfe0505cef05b107212f9bbc5
SimbaGithub <48035983+SimbaGithub@users.noreply.github.com>
2021-07-05 14:33:14 -0700

    Add SecretDetector and unit tests (#335)
    * Add SecretDetector and unit tests
    
    * Mask messages to be logged with SecretDetector
    
    * Add capability to set custom patterns for SecretDetector
    
    * Change to array param type when setting custom patterns
    
    * Include colon in AWS regex and add test for masking HTTP response
    
    * Add test for AWS key with single and double quotes

---
+++ Snowflake.Data.Tests/Mock/MockSecretDetector.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Copyright (c) 2021 Snowflake Computing Inc. All rights reserved.
+ */
+
+using System;
+using Snowflake.Data.Log;
+
+namespace Snowflake.Data.Tests.Mock
+{
+    class MockSecretDetector
+    {        
+        public static SecretDetector.Mask MaskSecrets(string text)
+        {
+            SecretDetector.Mask result = new SecretDetector.Mask();
+            try
+            {
+                throw new Exception("Test exception");
+            }
+            catch (Exception ex)
+            {
+                //We'll assume that the exception was raised during masking
+                //to be safe consider that the log has sensitive information
+                //and do not raise an exception.
+                result.isMasked = true;
+                result.maskedText = ex.Message;
+                result.errStr = ex.Message;
+            }
+            return result;
+        }
+    }
+}
