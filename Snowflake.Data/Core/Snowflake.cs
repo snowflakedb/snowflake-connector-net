d319df178c0fb5291db30c08e44b8dc5de091df5
SimbaGithub <SimbaGithub@magnitude.com>
2021-12-02 12:14:01 -0800

    Enable signed assembly

--- Snowflake.Data/Core/Snowflake.cs
+++ Snowflake.Data/Core/Snowflake.cs
@@ -3,4 +3,8 @@
  */
 
 using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Snowflake.Data.Tests")]
+[assembly: InternalsVisibleTo("Snowflake.Data.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001004d18acd7fd03b5" +
+"86ce065c44edc2f2539bebe19f2faf92ba93cc615ba39b547e9563491973fb6b57ea8c036c9ad9" +
+"80fb28dfc64689103da9d3e3644da36755a94ae88febfb74369b15e3405bf0a7ad6b18e74ecb2b" +
+"52f0ec913bfb4dc61ca1fde5ac19ef32d74eed9f76346122d414ddc8d7b0a6a407b211efedc56e" +
+"d3cb92c1")]
