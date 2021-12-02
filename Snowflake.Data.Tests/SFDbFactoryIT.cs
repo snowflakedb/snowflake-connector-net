4d8d8fa995d6ce1e0468ec8e68fcdec4116d17bd
SimbaGithub <SimbaGithub@magnitude.com>
2021-12-02 11:26:27 -0800

    Refactor tests

--- Snowflake.Data.Tests/SFDbFactoryIT.cs
+++ Snowflake.Data.Tests/SFDbFactoryIT.cs
@@ -5,12 +5,14 @@
 namespace Snowflake.Data.Tests
 {
     using NUnit.Framework;
+    using Snowflake.Data;
     using System.Data.Common;
 
     [TestFixture]
     class SFDbFactoryIT : SFBaseTest
     {
         [Test]
+        [Ignore("Ignore test on signed assembly")]
         public void TestSimpleDbFactory()
         {
 #if NETFRAMEWORK
@@ -19,7 +21,7 @@ namespace Snowflake.Data.Tests
             // In .NET Standard, DbProviderFactories is gone. 
             // Reference https://weblog.west-wind.com/posts/2017/Nov/27/Working-around-the-lack-of-dynamic-DbProviderFactory-loading-in-NET-Core
             // for more details
-            DbProviderFactory factory = Client.SnowflakeDbFactory.Instance;
+            DbProviderFactory factory = Snowflake.Data.Client.SnowflakeDbFactory.Instance;
 #endif
             DbCommand command = factory.CreateCommand();
             DbConnection connection = factory.CreateConnection();
