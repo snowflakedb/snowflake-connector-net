cf4a1544d28766753353952425f1b2196e143ba8
SimbaGithub <SimbaGithub@magnitude.com>
2022-02-03 09:40:07 -0800

    Remove unusued namespaces

--- Snowflake.Data/Core/FileTransfer/StorageClient/SFLocalStorageUtil.cs
+++ Snowflake.Data/Core/FileTransfer/StorageClient/SFLocalStorageUtil.cs
@@ -2,8 +2,6 @@
  * Copyright (c) 2012-2019 Snowflake Computing Inc. All rights reserved.
  */
 
-using Snowflake.Data.Core.FileTransfer.StorageClient;
-using System;
 using System.IO;
 
 namespace Snowflake.Data.Core.FileTransfer
