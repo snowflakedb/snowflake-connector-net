a4023e0309210e2b2b81a622fbfc0af7c7924dcd
SimbaGithub <48035983+SimbaGithub@users.noreply.github.com>
2022-10-14 10:15:40 -0700

    add keep alive for the connection (#530)
    * add keep alive for the connection, set "CLIENT_SESSION_KEEP_ALIVE=true" on the connection string to enable keep alive.
    
    * added alter session
    
    * adding ssession parameter for keep alive

--- Snowflake.Data/Core/RestResponse.cs
+++ Snowflake.Data/Core/RestResponse.cs
@@ -86,7 +86,9 @@ namespace Snowflake.Data.Core
         [JsonProperty(PropertyName = "sessionInfo", NullValueHandling = NullValueHandling.Ignore)]
         internal SessionInfo authResponseSessionInfo { get; set; }
 
-   }
+        [JsonProperty(PropertyName = "masterValidityInSeconds", NullValueHandling = NullValueHandling.Ignore)]
+        internal int masterValidityInSeconds { get; set; }
+    }
 
     internal class AuthenticatorResponseData
     {
