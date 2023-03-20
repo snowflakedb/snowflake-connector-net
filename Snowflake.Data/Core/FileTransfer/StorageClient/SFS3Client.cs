e8cae486397bd41cb506e21c4817af5519c3c4d1
Dicky Lau <dlau@magnitude.com>
2023-03-20 11:42:29 -0700

    adding https when the endpoint missing https

--- Snowflake.Data/Core/FileTransfer/StorageClient/SFS3Client.cs
+++ Snowflake.Data/Core/FileTransfer/StorageClient/SFS3Client.cs
@@ -304,6 +304,10 @@ namespace Snowflake.Data.Core.FileTransfer.StorageClient
                 if(start > -1 && end > -1 && end > start)
                 {
                     endpoint = endpoint.Substring(start + 1, end - start - 1);
+                    if(!endpoint.Contains("https"))
+                    {
+                        endpoint = "https://" + endpoint;
+                    }
                 }
                 clientConfig.ServiceURL = endpoint;
             }
