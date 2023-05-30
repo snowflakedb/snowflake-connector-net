9a42e34d0315eedffac35a7fd4d5be1ed78282b2
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2023-05-30 13:31:56 +0200

    SNOW-829461 Add AssertNet library to tests

--- Snowflake.Data.Tests/SFSessionPropertyTest.cs
+++ Snowflake.Data.Tests/SFSessionPropertyTest.cs
@@ -2,44 +2,35 @@
  * Copyright (c) 2019 Snowflake Computing Inc. All rights reserved.
  */
 
+using System.Collections.Generic;
 using Snowflake.Data.Core;
 using System.Security;
 using NUnit.Framework;
+using static AssertNet.Assertions;
 
 namespace Snowflake.Data.Tests
 {
-    /// <summary>
-    /// The purpose of these testcases is to test if the connections string
-    /// can be parsed correctly into properties for a session.
-    /// </summary>
     class SFSessionPropertyTest
     {
-        private class Testcase
-        {
-            public string ConnectionString { get; set; }
-            public SecureString SecurePassword { get; set; }
-            public SFSessionProperties ExpectedProperties { get; set; }
 
-            public void TestValidCase()
-            {
-                SFSessionProperties actualProperties = SFSessionProperties.parseConnectionString(ConnectionString, SecurePassword);
-                Assert.AreEqual(actualProperties, ExpectedProperties);
-            }
+        [Test, TestCaseSource("ConnectionStringTestCases")]
+        public void ShouldParseProperties(TestCase testcase)
+        {
+            // when
+            var properties = SFSessionProperties.parseConnectionString(
+                testcase.ConnectionString,
+                testcase.SecurePassword);
+
+            // then
+            AssertThat(properties)
+                .ContainsExactlyInAnyOrder(testcase.ExpectedProperties);
         }
-
-        [Test]
-        [Ignore("SessionPropertyTest")]
-        public void SessionPropertyTestDone()
+        
+        public static IEnumerable<TestCase> ConnectionStringTestCases()
         {
-            // Do nothing;
-        }
-
-        [Test]
-        public void TestValidConnectionString()
-        {
-            Testcase[] testcases = new Testcase[]
+            return new TestCase[]
             {
-                new Testcase()
+                new TestCase()
                 {
                     ConnectionString = "ACCOUNT=testaccount;USER=testuser;PASSWORD=123;",
                     ExpectedProperties = new SFSessionProperties()
@@ -59,14 +50,66 @@ namespace Snowflake.Data.Tests
                         { SFSessionProperty.FORCERETRYON404, "false" },
                         { SFSessionProperty.CLIENT_SESSION_KEEP_ALIVE, "false" },
                         { SFSessionProperty.FORCEPARSEERROR, "false" },
-                    },
+                    }
                 },
+                new TestCase() 
+                {
+                    ExpectedProperties = new SFSessionProperties()
+                    {
+                        { SFSessionProperty.ACCOUNT, "testaccount" },
+                        { SFSessionProperty.USER, "testuser" },
+                        { SFSessionProperty.HOST, "testaccount.snowflakecomputing.com" },
+                        { SFSessionProperty.AUTHENTICATOR, "snowflake" },
+                        { SFSessionProperty.SCHEME, "https" },
+                        { SFSessionProperty.CONNECTION_TIMEOUT, "120" },
+                        { SFSessionProperty.PASSWORD, "123" },
+                        { SFSessionProperty.PORT, "443" },
+                        { SFSessionProperty.VALIDATE_DEFAULT_PARAMETERS, "true" },
+                        { SFSessionProperty.INSECUREMODE, "false" },
+                        { SFSessionProperty.DISABLERETRY, "false" },
+                        { SFSessionProperty.FORCERETRYON404, "false" },
+                        { SFSessionProperty.CLIENT_SESSION_KEEP_ALIVE, "false" },
+                        { SFSessionProperty.FORCEPARSEERROR, "false" },
+                        { SFSessionProperty.USEPROXY, "true" },
+                        { SFSessionProperty.PROXYHOST, "proxy.com" },
+                        { SFSessionProperty.PROXYPORT, "1234" },
+                        { SFSessionProperty.NONPROXYHOSTS, "localhost" }
+                    },
+                    ConnectionString = "ACCOUNT=testaccount;USER=testuser;PASSWORD=123;useProxy=true;proxyHost=proxy.com;proxyPort=1234;nonProxyHosts=localhost"
+                },
+                new TestCase() 
+                {
+                    ExpectedProperties = new SFSessionProperties()
+                    {
+                        { SFSessionProperty.ACCOUNT, "testaccount" },
+                        { SFSessionProperty.USER, "testuser" },
+                        { SFSessionProperty.HOST, "testaccount.snowflakecomputing.com" },
+                        { SFSessionProperty.AUTHENTICATOR, "snowflake" },
+                        { SFSessionProperty.SCHEME, "https" },
+                        { SFSessionProperty.CONNECTION_TIMEOUT, "120" },
+                        { SFSessionProperty.PASSWORD, "123" },
+                        { SFSessionProperty.PORT, "443" },
+                        { SFSessionProperty.VALIDATE_DEFAULT_PARAMETERS, "true" },
+                        { SFSessionProperty.INSECUREMODE, "false" },
+                        { SFSessionProperty.DISABLERETRY, "false" },
+                        { SFSessionProperty.FORCERETRYON404, "false" },
+                        { SFSessionProperty.CLIENT_SESSION_KEEP_ALIVE, "false" },
+                        { SFSessionProperty.FORCEPARSEERROR, "false" },
+                        { SFSessionProperty.USEPROXY, "false" },
+                        { SFSessionProperty.PROXYHOST, "proxy.com" },
+                        { SFSessionProperty.PROXYPORT, "1234" },
+                        { SFSessionProperty.NONPROXYHOSTS, "localhost" }
+                    },
+                    ConnectionString = "ACCOUNT=testaccount;USER=testuser;PASSWORD=123;proxyHost=proxy.com;proxyPort=1234;nonProxyHosts=localhost"
+                }
             };
-
-            foreach (Testcase testcase in testcases)
-            {
-                testcase.TestValidCase();
-            }
+        }
+        
+        internal class TestCase
+        {
+            public string ConnectionString { get; set; }
+            public SecureString SecurePassword { get; set; }
+            public SFSessionProperties ExpectedProperties { get; set; }
         }
     }
 }
