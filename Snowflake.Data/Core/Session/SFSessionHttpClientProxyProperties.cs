0f17b708ad481f1358819025437e1f63c702c50c
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2023-06-06 15:54:50 +0200

    apply old namespace syntax

--- Snowflake.Data/Core/Session/SFSessionHttpClientProxyProperties.cs
+++ Snowflake.Data/Core/Session/SFSessionHttpClientProxyProperties.cs
@@ -1,43 +1,46 @@
 using System;
 using System.Web;
 
-namespace Snowflake.Data.Core;
-
-internal class SFSessionHttpClientProxyProperties
+namespace Snowflake.Data.Core
 {
-    internal string proxyHost = null;
-    internal string proxyPort = null;
-    internal string nonProxyHosts = null;
-    internal string proxyPassword = null;
-    internal string proxyUser = null;
 
-    internal interface IExtractor
+    internal class SFSessionHttpClientProxyProperties
     {
-        public SFSessionHttpClientProxyProperties ExtractProperties(SFSessionProperties propertiesDictionary);
-    }
-    
-    internal class Extractor: SFSessionHttpClientProxyProperties
-    {
-        public SFSessionHttpClientProxyProperties ExtractProperties(SFSessionProperties propertiesDictionary)
+        internal string proxyHost = null;
+        internal string proxyPort = null;
+        internal string nonProxyHosts = null;
+        internal string proxyPassword = null;
+        internal string proxyUser = null;
+
+        internal interface IExtractor
         {
-            var properties = new SFSessionHttpClientProxyProperties();
-            if (Boolean.Parse(propertiesDictionary[SFSessionProperty.USEPROXY]))
-            {
-                // Let's try to get the associated RestRequester
-                propertiesDictionary.TryGetValue(SFSessionProperty.PROXYHOST, out properties.proxyHost);
-                propertiesDictionary.TryGetValue(SFSessionProperty.PROXYPORT, out properties.proxyPort);
-                propertiesDictionary.TryGetValue(SFSessionProperty.NONPROXYHOSTS, out properties.nonProxyHosts);
-                propertiesDictionary.TryGetValue(SFSessionProperty.PROXYPASSWORD, out properties.proxyPassword);
-                propertiesDictionary.TryGetValue(SFSessionProperty.PROXYUSER, out properties.proxyUser);
+            public SFSessionHttpClientProxyProperties ExtractProperties(SFSessionProperties propertiesDictionary);
+        }
 
-                if (!String.IsNullOrEmpty(properties.nonProxyHosts))
+        internal class Extractor : SFSessionHttpClientProxyProperties
+        {
+            public SFSessionHttpClientProxyProperties ExtractProperties(SFSessionProperties propertiesDictionary)
+            {
+                var properties = new SFSessionHttpClientProxyProperties();
+                if (Boolean.Parse(propertiesDictionary[SFSessionProperty.USEPROXY]))
                 {
-                    // The list is url-encoded
-                    // Host names are separated with a URL-escaped pipe symbol (%7C). 
-                    properties.nonProxyHosts = HttpUtility.UrlDecode(properties.nonProxyHosts);
+                    // Let's try to get the associated RestRequester
+                    propertiesDictionary.TryGetValue(SFSessionProperty.PROXYHOST, out properties.proxyHost);
+                    propertiesDictionary.TryGetValue(SFSessionProperty.PROXYPORT, out properties.proxyPort);
+                    propertiesDictionary.TryGetValue(SFSessionProperty.NONPROXYHOSTS, out properties.nonProxyHosts);
+                    propertiesDictionary.TryGetValue(SFSessionProperty.PROXYPASSWORD, out properties.proxyPassword);
+                    propertiesDictionary.TryGetValue(SFSessionProperty.PROXYUSER, out properties.proxyUser);
+
+                    if (!String.IsNullOrEmpty(properties.nonProxyHosts))
+                    {
+                        // The list is url-encoded
+                        // Host names are separated with a URL-escaped pipe symbol (%7C). 
+                        properties.nonProxyHosts = HttpUtility.UrlDecode(properties.nonProxyHosts);
+                    }
                 }
+
+                return properties;
             }
-            return properties;
         }
     }
 }
