da52fd3042873ddb024c0c3cea3ecaf70cf3b020
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2023-06-14 18:00:12 +0200

    SNOW-839539 Add Moq library to mock objects in unit tests

--- Snowflake.Data/Core/Snowflake.cs
+++ Snowflake.Data/Core/Snowflake.cs
@@ -4,3 +4,4 @@
 
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Snowflake.Data.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // needed by Moq to be able to mock internal interfaces
