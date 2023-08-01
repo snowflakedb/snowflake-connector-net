49d6b1ccc58214d0e83c97ecccf273e0de8760cd
sfc-gh-ext-simba-lf <115584722+sfc-gh-ext-simba-lf@users.noreply.github.com>
2023-07-31 17:42:18 -0700

    Add tests for SFLogger and SnowflakeDbDataAdapter (#716)
    Regarding issue 113
    
    The PR adds tests for SFLogger and SnowflakeDbDataAdapter to increase
    code coverage

--- Snowflake.Data.Tests/SFDbAdaptorIT.cs
+++ Snowflake.Data.Tests/SFDbAdaptorIT.cs
@@ -7,17 +7,46 @@ namespace Snowflake.Data.Tests
     using NUnit.Framework;
     using Snowflake.Data.Client;
     using System.Data;
-    using System;
     using System.Runtime.InteropServices;
 
     [TestFixture]
     class SFDbAdaptorIT : SFBaseTest
     {
+        IDbDataAdapter _adapter;
+        SnowflakeDbCommand _command;
+
+        [Test]
+        public void BeforeTest()
+        {
+            _adapter = new SnowflakeDbDataAdapter();
+            _command = new SnowflakeDbCommand();
+        }
+
         [Test]
         [Ignore("DbAdaptorIT")]
         public void DbAdaptorITDone()
         {
             // Do nothing;
+        }
+
+        [Test]
+        public void TestCreatingDataAdapterWithSelectCommand()
+        {
+            _command.CommandText = "select 1 as col1, 2 AS col2";
+            _adapter = new SnowflakeDbDataAdapter(_command);
+
+            Assert.AreEqual(_command.CommandText, _adapter.SelectCommand.CommandText);
+        }
+
+        [Test]
+        public void TestCreatingDataAdapterWithSelectCommandTextAndConnection()
+        {
+            _command.CommandText = "select 1 as col1, 2 AS col2";
+            SnowflakeDbConnection conn = new SnowflakeDbConnection(ConnectionString);
+            _adapter = new SnowflakeDbDataAdapter(_command.CommandText, conn);
+
+            Assert.AreEqual(_command.CommandText, _adapter.SelectCommand.CommandText);
+            Assert.AreEqual(conn, _adapter.SelectCommand.Connection);
         }
 
         [Test]
@@ -29,8 +58,8 @@ namespace Snowflake.Data.Tests
                 conn.ConnectionString = ConnectionString;
                 conn.Open();
 
-                IDbDataAdapter adaptor = new SnowflakeDbDataAdapter("select 1 as col1, 2 AS col2", conn);
-                adaptor.Fill(ds);
+                _adapter = new SnowflakeDbDataAdapter("select 1 as col1, 2 AS col2", conn);
+                _adapter.Fill(ds);
                 conn.Close();
             }
             Assert.AreEqual(ds.Tables[0].TableName, "Table");
@@ -42,6 +71,46 @@ namespace Snowflake.Data.Tests
                 Assert.AreEqual(ds.Tables[0].Rows[0]["col1"].ToString(), "1");
                 Assert.AreEqual(ds.Tables[0].Rows[0]["col2"].ToString(), "2");
             }
+        }
+
+        [Test]
+        public void TestDataAdapterSetDeleteCommand()
+        {
+            _command.CommandText = "delete from table";
+            _adapter.DeleteCommand = _command;
+
+            Assert.AreEqual(_command, _adapter.DeleteCommand);
+            Assert.AreEqual(_command.CommandText, _adapter.DeleteCommand.CommandText);
+        }
+
+        [Test]
+        public void TestDataAdapterSetInsertCommand()
+        {
+            _command.CommandText = "insert into table values (1, 2, 3)";
+            _adapter.InsertCommand = _command;
+
+            Assert.AreEqual(_command, _adapter.InsertCommand);
+            Assert.AreEqual(_command.CommandText, _adapter.InsertCommand.CommandText);
+        }
+
+        [Test]
+        public void TestDataAdapterSetSelectCommand()
+        {
+            _command.CommandText = "select 1 as col1, 2 AS col2";
+            _adapter.SelectCommand = _command;
+
+            Assert.AreEqual(_command, _adapter.SelectCommand);
+            Assert.AreEqual(_command.CommandText, _adapter.SelectCommand.CommandText);
+        }
+
+        [Test]
+        public void TestDataAdapterSetUpdateCommand()
+        {
+            _command.CommandText = "update table set col = 1 where col = 0";
+            _adapter.UpdateCommand = _command;
+
+            Assert.AreEqual(_command, _adapter.UpdateCommand);
+            Assert.AreEqual(_command.CommandText, _adapter.UpdateCommand.CommandText);
         }
     }
 }
