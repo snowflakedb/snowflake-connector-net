180de7105c68527eb57f9916d14239d24c09decc
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2023-08-21 09:56:54 -0700

    SNOW-723810: Fix merge conflicts for SFDbFactory

--- Snowflake.Data.Tests/IntegrationTests/SFDbFactoryIT.cs
+++ Snowflake.Data.Tests/IntegrationTests/SFDbFactoryIT.cs
@@ -2,15 +2,44 @@
  * Copyright (c) 2012-2019 Snowflake Computing Inc. All rights reserved.
  */
 
-namespace Snowflake.Data.Tests
+namespace Snowflake.Data.Tests.IntegrationTests
 {
     using NUnit.Framework;
-    using Snowflake.Data;
+    using System.Data;
     using System.Data.Common;
 
     [TestFixture]
     class SFDbFactoryIT : SFBaseTest
     {
+        DbProviderFactory _factory;
+        DbCommand _command;
+        DbConnection _connection;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+#if NETFRAMEWORK
+            _factory = DbProviderFactories.GetFactory("Snowflake.Data");
+#else
+            // In .NET Standard, DbProviderFactories is gone.
+            // Reference https://weblog.west-wind.com/posts/2017/Nov/27/Working-around-the-lack-of-dynamic-DbProviderFactory-loading-in-NET-Core
+            // for more details
+            _factory = Client.SnowflakeDbFactory.Instance;
+#endif
+
+            _command = _factory.CreateCommand();
+            _connection = _factory.CreateConnection();
+
+            _connection.ConnectionString = ConnectionString;
+            _connection.Open();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            _connection.Close();
+        }
+
         [Test]
         [Ignore("DbFactoryIT")]
         public void DbFactoryITDone()
@@ -21,20 +50,11 @@ namespace Snowflake.Data.Tests
         [Test]
         public void TestSimpleDbFactory()
         {
-#if NETFRAMEWORK
-            DbProviderFactory factory = DbProviderFactories.GetFactory("Snowflake.Data");
-#else
-            // In .NET Standard, DbProviderFactories is gone. 
-            // Reference https://weblog.west-wind.com/posts/2017/Nov/27/Working-around-the-lack-of-dynamic-DbProviderFactory-loading-in-NET-Core
-            // for more details
-            DbProviderFactory factory = Snowflake.Data.Client.SnowflakeDbFactory.Instance;
-#endif
-            DbCommand command = factory.CreateCommand();
-            DbConnection connection = factory.CreateConnection();
-            connection.ConnectionString = ConnectionString;
-            connection.Open();
             // set commnad's connection object
-            command.Connection = connection;
+            _command.Connection = _connection;
+            _command.CommandText = "select 1";
+
+            object res = _command.ExecuteScalar();
 
             Assert.AreEqual(1, res);
         }
