dc4beffdaaa615414230acc179d32ca6d1c60d83
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2023-08-31 10:21:37 +0200

    SNOW-888701: Fix uploading with compression (#741)
    ### Description
    - Fix uploading non compressed file with auto_compress option to Azure
    - Fix uploading and downloading non compressed files with auto_compress
    option with non empty path on the server
    
    ### Checklist
    - [x] Code compiles correctly
    - [x] Code is formatted according to [Coding
    Conventions](../CodingConventions.md)
    - [x] Created tests which fail without the change (if possible)
    - [x] All tests passing (`dotnet test`)
    - [x] Extended the README / documentation, if necessary
    - [x] Provide JIRA issue id (if possible) or GitHub issue id in PR name

--- Snowflake.Data/Core/FileTransfer/StorageClient/SFSnowflakeAzureClient.cs
+++ Snowflake.Data/Core/FileTransfer/StorageClient/SFSnowflakeAzureClient.cs
@@ -102,7 +102,7 @@ namespace Snowflake.Data.Core.FileTransfer.StorageClient
 
             // Get the Azure client
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(location.bucket);
-            BlobClient blobClient = containerClient.GetBlobClient(location.key + fileMetadata.srcFileName);
+            BlobClient blobClient = containerClient.GetBlobClient(location.key + fileMetadata.RemoteFileName());
 
             BlobProperties response;
             try
@@ -131,7 +131,7 @@ namespace Snowflake.Data.Core.FileTransfer.StorageClient
 
             // Get the Azure client
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(location.bucket);
-            BlobClient blobClient = containerClient.GetBlobClient(location.key + fileMetadata.srcFileName);
+            BlobClient blobClient = containerClient.GetBlobClient(location.key + fileMetadata.RemoteFileName());
 
             BlobProperties response;
             try
@@ -274,7 +274,7 @@ namespace Snowflake.Data.Core.FileTransfer.StorageClient
 
             // Get the Azure client
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(location.bucket);
-            return containerClient.GetBlobClient(location.key + fileMetadata.srcFileName);
+            return containerClient.GetBlobClient(location.key + fileMetadata.destFileName);
         }
 
         /// <summary>
