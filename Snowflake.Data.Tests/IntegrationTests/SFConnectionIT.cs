53a6df1b68e94e787ba5297cf71534e17e93f8b5
Piotr Bulawa <piotr.bulawa@snowflake.com>
2023-09-04 23:31:32 +0200

    Set test to be non-parallelizable

--- Snowflake.Data.Tests/IntegrationTests/SFConnectionIT.cs
+++ Snowflake.Data.Tests/IntegrationTests/SFConnectionIT.cs
@@ -379,7 +379,7 @@ namespace Snowflake.Data.Tests.IntegrationTests
             }
         }
 
-        [Test]
+        [Test, NonParallelizable]
         public void TestLoginTimeout()
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
