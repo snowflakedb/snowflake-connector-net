220a9c14ed002d276a1fbdfbed2ed9303743a2a6
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2023-09-26 16:25:23 -0700

    SNOW-918411: Configure to sign assembly

--- Snowflake.Data/Core/Snowflake.cs
+++ Snowflake.Data/Core/Snowflake.cs
@@ -3,5 +3,16 @@
  */
 
 using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Snowflake.Data.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // needed by Moq to be able to mock internal interfaces
+[assembly: InternalsVisibleTo("Snowflake.Data.Tests, PublicKey=" +
+    "0024000004800000940000000602000000240000525341310004000001000100d90268f96e9542" +
+    "615b6dbfddb1ad6e03f05923ee64c43af1b189a58710da5565f2e229ce9371e646da9eabbee97d" +
+    "83aaf8d93dd61c0a9788b239a026bb1824b0c622a5f405fb89083bf823143e98048828e2ac5792" +
+    "0c840de0e914c017cd0bb8836e41ad0018b3613a6e87e57467569a5a1330d1a7f9e7a560fcca2f" +
+    "5db936cc")]
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2,PublicKey=" + // needed by Moq to be able to mock internal interfaces
+    "0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99" +
+    "c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654" +
+    "753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46" +
+    "ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484c" +
+    "f7045cc7")]
