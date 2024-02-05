bd5d8da929b9f154f8ebd6e42d1f8dad267cf12c
sfc-gh-ext-simba-lf <115584722+sfc-gh-ext-simba-lf@users.noreply.github.com>
2024-02-05 09:02:21 -0800

    SNOW-990111: Add easy logging improvements (#849)
    ### Description
    Modify easy logging based on list of security improvements
    
    ### Checklist
    - [x] Code compiles correctly
    - [x] Code is formatted according to [Coding
    Conventions](../CodingConventions.md)
    - [x] Created tests which fail without the change (if possible)
    - [x] All tests passing (`dotnet test`)
    - [x] Extended the README / documentation, if necessary
    - [x] Provide JIRA issue id (if possible) or GitHub issue id in PR name

---
+++ Snowflake.Data/Core/Tools/HomeDirectoryProvider.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Copyright (c) 2024 Snowflake Computing Inc. All rights reserved.
+ */
+
+using Snowflake.Data.Log;
+using System;
+
+namespace Snowflake.Data.Core.Tools
+{
+    internal class HomeDirectoryProvider
+    {
+        private static readonly SFLogger s_logger = SFLoggerFactory.GetLogger<HomeDirectoryProvider>();
+
+        public static string HomeDirectory(EnvironmentOperations _environmentOperations) {
+            try
+            {
+                var homeDirectory = _environmentOperations.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                if (string.IsNullOrEmpty(homeDirectory) || homeDirectory.Equals("/"))
+                {
+                    return null;
+                }
+                return homeDirectory;
+            }
+            catch (Exception e)
+            {
+                s_logger.Error($"Error while trying to retrieve the home directory: {e}");
+                return null;
+            }
+        }
+    }
+}
