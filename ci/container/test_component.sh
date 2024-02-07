904bb0a104e327f7481bf2e0ced43c94ce8a1553
Adam Kolodziejczyk <adam.kolodziejczyk@snowflake.com>
2024-02-07 17:20:37 +0100

    change report directory

--- ci/container/test_component.sh
+++ ci/container/test_component.sh
@@ -5,4 +5,4 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SOURCE_ROOT=${SOURCE_ROOT:-/mnt/host}
 
 cd $SOURCE_ROOT
-dotnet test -f net6.0 -l "console;verbosity=normal" --logger:"junit;LogFilePath=../junit-dotnet.xml"
+dotnet test -f net6.0 -l "console;verbosity=normal" --logger:"junit;LogFilePath=$SOURCE_ROOT/junit-dotnet.xml"
