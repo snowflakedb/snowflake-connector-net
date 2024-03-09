97904a967ab9ba63bab900fe04b17c322d6f653e
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2024-03-08 18:48:00 -0800

    Revert last commit

--- Snowflake.Data.Tests/IntegrationTests/SFDbCommandIT.cs
+++ Snowflake.Data.Tests/IntegrationTests/SFDbCommandIT.cs
@@ -516,7 +516,7 @@ namespace Snowflake.Data.Tests.IntegrationTests
             }
         }
 
-        [Test, NonParallelizable]
+        [Test]
         public void TestGetResultsOfUnknownQueryIdAsync()
         {
             string unknownQueryId = "ab123fed-1abc-987f-987f-1234a56b789c";
@@ -1669,7 +1669,7 @@ namespace Snowflake.Data.Tests.IntegrationTests
             }
         }
 
-        [Test, NonParallelizable]
+        [Test]
         public void TestGetResultsOfUnknownQueryId()
         {
             string unknownQueryId = "ba987def-1abc-987f-987f-1234a56b789c";
