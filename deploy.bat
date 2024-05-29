2119080ab3f84be658b92cae0deae415ceb7f9c2
Juan Martinez Ramirez <126511805+sfc-gh-jmartinez@users.noreply.github.com>
2024-05-29 16:25:19 -0600

    SNOW-991583: Assembly delay sign (#956)
    ### Description
    Add delay sign for Snowflake Data assemblies.
    
    ### Checklist
    - [x] Code compiles correctly
    - [x] Code is formatted according to [Coding
    Conventions](../blob/master/CodingConventions.md)
    - [x] Created tests which fail without the change (if possible)
    - [x] All tests passing (`dotnet test`)
    - [x] Extended the README / documentation, if necessary
    - [x] Provide JIRA issue id (if possible) or GitHub issue id in PR name

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,10 @@ SET API_KEY=%2
 SET ROOT_DIR=%~dp0 
 cd %ROOT_DIR%
 
-dotnet pack Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n --output %ROOT_DIR%
+dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n 
+
+REM command to sign with strong name Snowflake.Data.dll should be here
+
+dotnet pack Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n --no-build  --output %ROOT_DIR%
 
 dotnet nuget push Snowflake.Data.%VERSION%.nupkg -k %API_KEY% -s https://api.nuget.org/v3/index.json
