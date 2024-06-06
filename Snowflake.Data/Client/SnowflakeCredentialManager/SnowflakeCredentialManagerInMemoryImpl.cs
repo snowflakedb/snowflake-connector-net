739c40c72006cfeb0e6dd367b9fdb7b37ef7f83e
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2024-06-05 18:01:13 -0700

    Remove encryption for in-memory credential manager

--- Snowflake.Data/Client/SnowflakeCredentialManager/SnowflakeCredentialManagerInMemoryImpl.cs
+++ Snowflake.Data/Client/SnowflakeCredentialManager/SnowflakeCredentialManagerInMemoryImpl.cs
@@ -4,83 +4,29 @@
 
 using Snowflake.Data.Log;
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace Snowflake.Data.Client
 {
-    public struct EncryptedToken
-    {
-        public byte[] IV;
-        public byte[] Key;
-        public byte[] Bytes;
-    }
-
-    internal class SnowflakeCredentialEncryption
-    {
-        internal static EncryptedToken EncryptToken(string token)
-        {
-            EncryptedToken encryptedToken;
-
-            using (var aes = Aes.Create())
-            {
-                aes.Mode = CipherMode.CBC;
-                aes.BlockSize = 128;
-                aes.GenerateKey();
-                encryptedToken.Key = aes.Key;
-                aes.GenerateIV();
-                encryptedToken.IV = aes.IV;
-
-                using (var encryptor = aes.CreateEncryptor())
-                {
-                    var tokenBytes = Encoding.UTF8.GetBytes(token);
-                    encryptedToken.Bytes = encryptor.TransformFinalBlock(tokenBytes, 0, tokenBytes.Length);
-                }
-            }
-
-            return encryptedToken;
-        }
-
-        internal static string DecryptToken(EncryptedToken encryptedToken)
-        {
-            using (var aes = Aes.Create())
-            {
-                aes.BlockSize = 128;
-                aes.Mode = CipherMode.CBC;
-                aes.Key = encryptedToken.Key;
-                aes.IV = encryptedToken.IV;
-
-                using (var decryptor = aes.CreateDecryptor())
-                {
-                    var decryptedBytes = decryptor.TransformFinalBlock(encryptedToken.Bytes, 0, encryptedToken.Bytes.Length);
-                    return Encoding.UTF8.GetString(decryptedBytes);
-                }
-            }
-        }
-    }
-
     public class SnowflakeCredentialManagerInMemoryImpl : ISnowflakeCredentialManager
     {
         private static readonly SFLogger s_logger = SFLoggerFactory.GetLogger<SnowflakeCredentialManagerInMemoryImpl>();
 
-        private static readonly Dictionary<string, EncryptedToken> s_credentials = new Dictionary<string, EncryptedToken>();
+        private static readonly Dictionary<string, string> s_credentials = new Dictionary<string, string>();
 
         public static readonly SnowflakeCredentialManagerInMemoryImpl Instance = new SnowflakeCredentialManagerInMemoryImpl();
 
         public string GetCredentials(string key)
         {
-            EncryptedToken token;
-            s_credentials.TryGetValue(key, out token);
-
             s_logger.Debug($"Getting credentials from memory for key: {key}");
-            if (token.Bytes == null)
+            string token;
+            if (s_credentials.TryGetValue(key, out token))
             {
-                s_logger.Info("Unable to get credentials for the specified key");
-                return "";
+                return token;
             }
             else
             {
-                return SnowflakeCredentialEncryption.DecryptToken(s_credentials[key]);
+                s_logger.Info("Unable to get credentials for the specified key");
+                return "";
             }
         }
 
@@ -93,7 +39,7 @@ namespace Snowflake.Data.Client
         public void SaveCredentials(string key, string token)
         {
             s_logger.Debug($"Saving credentials into memory for key: {key}");
-            s_credentials[key] = SnowflakeCredentialEncryption.EncryptToken(token);
+            s_credentials[key] = token;
         }
     }
 }
