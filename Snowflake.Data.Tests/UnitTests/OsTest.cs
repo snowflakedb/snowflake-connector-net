f96faf0638cf5f45b45345ec60c1d07d56595e03
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2024-06-10 11:05:55 +0000

    debugging

---
+++ Snowflake.Data.Tests/UnitTests/OsTest.cs
@@ -0,0 +1,24 @@
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+using Snowflake.Data.Core.Tools;
+using Snowflake.Data.Log;
+
+namespace Snowflake.Data.Tests.UnitTests
+{
+    [TestFixture]
+    public class OsTest
+    {
+        private static readonly SFLogger s_logger = SFLoggerFactory.GetLogger<EnvironmentOperations>();
+
+        [Test]
+        public void TestOs()
+        {
+            // var environment = EnvironmentOperations.Instance;
+            // var osVariable = environment.GetEnvironmentVariable("OS");
+            // s_logger.Warn($"OS: {osVariable}!!!");
+            // // s_logger.Warn($"OS Description: {RuntimeInformation.OSDescription}!!!");
+            // s_logger.Warn($"OS RuntimeIdentifier: {RuntimeInformation.RuntimeIdentifier}!!!");
+            // s_logger.Warn($"OS OSArchitecture: {RuntimeInformation.OSArchitecture}!!!");
+        }
+    }
+}
