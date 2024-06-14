5dab14b8bd52d57b5b62e2c3ff7092f32267dc8a
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-06-14 13:41:34 +0530

    Checking for sn key3

--- deploy.bat
+++ deploy.bat
@@ -12,7 +12,7 @@ REM command to sign with strong name Snowflake.Data.dll should be here
 where sn.exe
 
 dir "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"
-sn -R obj\Release\net8.0\Snowflake.Data.dll %SN_KEY%
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -R obj\Release\net8.0\Snowflake.Data.dll %SN_KEY%
 
 dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile="C:\jenkins\workspace\NugetPushDotNetDriverSignTest\key.snk"
 
