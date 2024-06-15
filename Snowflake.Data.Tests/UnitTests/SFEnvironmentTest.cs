722de1531284944298c0a1aae8bd60cc7b553d4b
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2024-06-14 20:39:58 -0700

    Comment out RuntimeFrameworkVersion

--- Snowflake.Data.Tests/UnitTests/SFEnvironmentTest.cs
+++ Snowflake.Data.Tests/UnitTests/SFEnvironmentTest.cs
@@ -10,14 +10,17 @@ namespace Snowflake.Data.Tests.UnitTests
         public void TestRuntimeExtraction()
         {
             // Arrange
-            string expectedRuntime;
+            string expectedRuntime = ".NET";
             string expectedVersion;
 
 #if NETFRAMEWORK
-            expectedRuntime = ".NETFramework";
+            expectedRuntime += "Framework";
             expectedVersion = "4.8";
-#else
-            expectedRuntime = ".NET";
+#elif NET6_0
+            expectedVersion = "6.0";
+#elif NET7_0
+            expectedVersion = "7.0";
+#elif NET8_0
             expectedVersion = "8.0";
 #endif
 
