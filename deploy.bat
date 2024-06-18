57a238d07eae056922b369185403c54d930aa3c4
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-06-18 11:58:02 +0530

    Added encoded decoded

--- deploy.bat
+++ deploy.bat
@@ -11,8 +11,10 @@ echo %SN_KEY% >> "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt"
 
 certutil -f  -decode "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt" "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\key.snk"
 
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -k "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\key.snk"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -k "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkey.snk"
+certutil -f  -encode "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkey.snk" "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkeyencoded.txt"
+certutil -f  -decode "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkeyencoded.txt" "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkeydecoded.snk"
 
-dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile="C:\jenkins\workspace\NugetPushDotNetDriverSignTest\key.snk"
+dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile="C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkeydecoded.snk"
 
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\Snowflake.Data\bin\Release\net8.0\Snowflake.Data.dll"
