6ca7cecfb8637fe1294e35955eae60a4c4e1fb04
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2024-06-18 17:56:34 -0700

    Change test to use assembly for expected values

--- Snowflake.Data.Tests/UnitTests/SFEnvironmentTest.cs
+++ Snowflake.Data.Tests/UnitTests/SFEnvironmentTest.cs
@@ -1,5 +1,8 @@
 using NUnit.Framework;
 using Snowflake.Data.Core;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
 
 namespace Snowflake.Data.Tests.UnitTests
 {
@@ -10,30 +13,15 @@ namespace Snowflake.Data.Tests.UnitTests
         public void TestRuntimeExtraction()
         {
             // Arrange
-            string expectedRuntime = ".NET";
-            string expectedVersion;
+            var targetFrameworkAttribute = Assembly.GetExecutingAssembly().GetCustomAttributes(true).ToList().OfType<TargetFrameworkAttribute>().First();
+            var targetFramework = targetFrameworkAttribute.FrameworkName.Split(',');
+            var expectedRuntime = targetFramework[0].Replace("CoreApp", "");
+            var expectedVersion = targetFramework[1].Substring(targetFramework[1].IndexOf("=v") + 2);
 
-#if NETFRAMEWORK
-            expectedRuntime += "Framework";
-#endif
-
-#if NET462
-            expectedVersion = "462";
-#elif NET471
-            expectedVersion = "471";
-#elif NET472
-            expectedVersion = "472";
-#elif NET48
-            expectedVersion = "48";
-#elif NET481
-            expectedVersion = "481";
-#elif NET6_0
-            expectedVersion = "6.0";
-#elif NET7_0
-            expectedVersion = "7.0";
-#elif NET8_0
-            expectedVersion = "8.0";
-#endif
+            if (expectedRuntime.Contains("Framework"))
+            {
+                expectedVersion = expectedVersion.Replace(".", "");
+            }
 
             // Act
             var actualRuntime = SFEnvironment.ExtractRuntime();
