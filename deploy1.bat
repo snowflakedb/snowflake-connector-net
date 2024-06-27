faa0fad3c9bbfb53e22c00d5902d55939e357eea
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-06-27 07:51:52 +0530

    Fix s3 command

--- deploy1.bat
+++ deploy1.bat
@@ -1,10 +1,12 @@
+cd "C:\jenkins\workspace\DotNetKeyTest"
+
 SETLOCAL EnableDelayedExpansion
 
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -k "C:\jenkins\workspace\DotNetKeyTest\newkey.snk"
 
 certutil -f -encode "C:\jenkins\workspace\DotNetKeyTest\newkey.snk" "C:\jenkins\workspace\DotNetKeyTest\newkey.snk.asc"
 
-for /f "Tokens=* Delims=" %%x in ("C:\jenkins\workspace\DotNetKeyTest\newkey.snk.asc") do set Content=!Content!%%x
+for /f "Tokens=* Delims=" %%x in (newkey.snk.asc) do set Content=!Content!%%x
 
 echo %Content% > "C:\jenkins\workspace\DotNetKeyTest\snk.txt"
 
