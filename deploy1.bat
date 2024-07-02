4f7244fba6f7afe8754e99d7afe321fbbbe661d6
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-07-02 16:14:10 +0530

    Strip begin and last

--- deploy1.bat
+++ deploy1.bat
@@ -6,8 +6,10 @@ SETLOCAL EnableDelayedExpansion
 
 certutil -f -encode "C:\jenkins\workspace\DotNetKeyTest\newkey.snk" "C:\jenkins\workspace\DotNetKeyTest\newkey.snk.asc"
 
-for /f "Tokens=* Delims=" %%x in (newkey.snk.asc) do set Content=!Content!%%x
+more +1 "C:\jenkins\workspace\DotNetKeyTest\newkey.snk.asc"|findstr /vX "-----END CERTIFICATE-----" > "C:\jenkins\workspace\DotNetKeyTest\newkey1.snk.asc"
+
+for /f "Tokens=* Delims=" %%x in (newkey1.snk.asc) do set Content=!Content!%%x
 
 echo %Content% > "C:\jenkins\workspace\DotNetKeyTest\snk.txt"
 
-aws s3 cp "C:\jenkins\workspace\DotNetKeyTest\snk.txt" "s3://sfc-eng-jenkins/repository/python_connector/ank.txt"
+aws s3 cp "C:\jenkins\workspace\DotNetKeyTest\snk.txt" "s3://sfc-eng-jenkins/repository/python_connector/ank1.txt"
