5ad2ea80daeeca9cd3dfa038da520e5c2f117fa0
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-07-02 19:06:07 +0530

    Test strip1

--- deploy1.bat
+++ deploy1.bat
@@ -1,6 +1,7 @@
 cd "C:\jenkins\workspace\DotNetKeyTest"
 
 SETLOCAL EnableDelayedExpansion
+SET Footer="-----END CERTIFICATE-----"
 
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -k "C:\jenkins\workspace\DotNetKeyTest\newkey.snk"
 
@@ -8,8 +9,10 @@ certutil -f -encode "C:\jenkins\workspace\DotNetKeyTest\newkey.snk" "C:\jenkins\
 
 more +1 "C:\jenkins\workspace\DotNetKeyTest\newkey.snk.asc" > "C:\jenkins\workspace\DotNetKeyTest\newkey1.snk.asc"
 
-for /f "Tokens=* Delims=" %%x in (newkey1.snk.asc) do set Content=!Content!%%x
+FINDSTR /R /I /V "^$ Footer" "C:\jenkins\workspace\DotNetKeyTest\newkey1.snk.asc">>"C:\jenkins\workspace\DotNetKeyTest\newkey2.snk.asc"
+
+for /f "Tokens=* Delims=" %%x in (newkey2.snk.asc) do set Content=!Content!%%x
 
 echo %Content% > "C:\jenkins\workspace\DotNetKeyTest\snk.txt"
 
-aws s3 cp "C:\jenkins\workspace\DotNetKeyTest\snk.txt" "s3://sfc-eng-jenkins/repository/python_connector/ank1.txt"
+aws s3 cp "C:\jenkins\workspace\DotNetKeyTest\snk.txt" "s3://sfc-eng-jenkins/repository/python_connector/ank2.txt"
