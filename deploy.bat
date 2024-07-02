6c33ea16eec39e4110597ea5bf1cfae9240c6679
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-07-02 19:47:47 +0530

    Remove commented out lines

--- deploy.bat
+++ deploy.bat
@@ -10,15 +10,8 @@ echo -----BEGIN CERTIFICATE----- > "C:\jenkins\workspace\NugetPushDotNetDriverSi
 echo %LINE1% >> "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt"
 echo -----END CERTIFICATE----- >> "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt"
 
-type "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt"
 
 certutil -decode "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt" "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\key.snk"
-
-type "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\key.snk"
-
-:: "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -k "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkey.snk"
-:: certutil -f  -encode "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkey.snk" "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkeyencoded.txt"
-:: certutil -f  -decode "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkeyencoded.txt" "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\newkeydecoded.snk"
 
 dir "C:\jenkins\workspace\NugetPushDotNetDriverSignTest"
 
