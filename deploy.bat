daddcf8cfb97d173c19f123cb0ad23d5c27285c3
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-07-03 14:55:36 +0530

    Change to relative path

--- deploy.bat
+++ deploy.bat
@@ -6,14 +6,14 @@ SET SNKEY=%3
 SET ROOT_DIR=%~dp0 
 cd %ROOT_DIR%
 
-echo -----BEGIN CERTIFICATE----- > "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt"
-echo %SNKEY% >> "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt"
-echo -----END CERTIFICATE----- >> "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt"
+echo -----BEGIN CERTIFICATE----- > %WORKSPACE%\coded.txt
+echo %SNKEY% >> %WORKSPACE%\coded.txt
+echo -----END CERTIFICATE----- >> %WORKSPACE%\coded.txt
 
-certutil -decode "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\coded.txt" "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\key.snk"
+certutil -decode %WORKSPACE%\coded.txt %WORKSPACE%\key.snk
 
-dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile="C:\jenkins\workspace\NugetPushDotNetDriverSignTest\key.snk" 
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v "C:\jenkins\workspace\NugetPushDotNetDriverSignTest\Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
+dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%WORKSPACE%\key.snk 
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v %WORKSPACE%\"Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
 
 dotnet pack Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n --no-build  --output %ROOT_DIR%
 
