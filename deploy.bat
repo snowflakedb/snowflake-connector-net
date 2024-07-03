d1dad1cb0428a20bcf0b3e369e912e6f7998d824
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-07-03 17:11:27 +0530

    Snow 991583 strong assembly sign (#987)
    ### Description
    .NET driver signing assembly with strong name
    
    ### Checklist
    - [ ] Code compiles correctly
    - [ ] Code is formatted according to [Coding
    Conventions](../blob/master/CodingConventions.md)
    - [ ] Created tests which fail without the change (if possible)
    - [ ] All tests passing (`dotnet test`)
    - [ ] Extended the README / documentation, if necessary
    - [ ] Provide JIRA issue id (if possible) or GitHub issue id in PR name

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,19 @@
 REM Scripts to build .net driver and deploy
 SET VERSION=%1
 SET API_KEY=%2
+SET SNKEY=%3
 
 SET ROOT_DIR=%~dp0 
 cd %ROOT_DIR%
 
-dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n 
+echo -----BEGIN CERTIFICATE----- > %WORKSPACE%\coded.txt
+echo %SNKEY% >> %WORKSPACE%\coded.txt
+echo -----END CERTIFICATE----- >> %WORKSPACE%\coded.txt
 
-REM command to sign with strong name Snowflake.Data.dll should be here
+certutil -decode %WORKSPACE%\coded.txt %WORKSPACE%\key.snk
+
+dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%WORKSPACE%\key.snk 
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v %WORKSPACE%"\Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
 
 dotnet pack Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n --no-build  --output %ROOT_DIR%
 
