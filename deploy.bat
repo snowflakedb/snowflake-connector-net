0217fe79054553faa57aee9603f013d7f45e84e3
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2024-07-03 15:27:54 +0530

    Fix Typo

--- deploy.bat
+++ deploy.bat
@@ -13,5 +13,5 @@ echo -----END CERTIFICATE----- >> %WORKSPACE%\coded.txt
 certutil -decode %WORKSPACE%\coded.txt %WORKSPACE%\key.snk
 
 dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%WORKSPACE%\key.snk 
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v %WORKSPACE%\"Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v %WORKSPACE%"\Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
 
