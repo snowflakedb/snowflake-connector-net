2ba9aaf028f8527041a40888b81e212c692ec7f8
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2024-07-08 15:18:23 -0700

    Remove printing port number and add check if result is completed

--- Snowflake.Data/Core/Authenticator/ExternalBrowserAuthenticator.cs
+++ Snowflake.Data/Core/Authenticator/ExternalBrowserAuthenticator.cs
@@ -79,7 +79,6 @@ namespace Snowflake.Data.Core.Authenticator
             if (string.IsNullOrEmpty(session._idToken))
             {
                 int localPort = GetRandomUnusedPort();
-                Console.WriteLine("External browser port number: " + localPort);
                 using (var httpListener = GetHttpListener(localPort))
                 {
                     httpListener.Start();
@@ -163,24 +162,28 @@ namespace Snowflake.Data.Core.Authenticator
             {
                 try
                 {
-                    HttpListenerContext context = httpListener.EndGetContext(result);
-                    HttpListenerRequest request = context.Request;
-
-                    _samlResponseToken = ValidateAndExtractToken(request);
-                    if (!string.IsNullOrEmpty(_samlResponseToken))
+                    Console.WriteLine("External browser result completed: " + result.IsCompleted);
+                    if (result.IsCompleted)
                     {
-                        HttpListenerResponse response = context.Response;
-                        try
+                        HttpListenerContext context = httpListener.EndGetContext(result);
+                        HttpListenerRequest request = context.Request;
+
+                        _samlResponseToken = ValidateAndExtractToken(request);
+                        if (!string.IsNullOrEmpty(_samlResponseToken))
                         {
-                            using (var output = response.OutputStream)
+                            HttpListenerResponse response = context.Response;
+                            try
                             {
-                                output.Write(SUCCESS_RESPONSE, 0, SUCCESS_RESPONSE.Length);
+                                using (var output = response.OutputStream)
+                                {
+                                    output.Write(SUCCESS_RESPONSE, 0, SUCCESS_RESPONSE.Length);
+                                }
                             }
-                        }
-                        catch
-                        {
-                            // Ignore the exception as it does not affect the overall authentication flow
-                            logger.Warn("External browser response not sent out");
+                            catch
+                            {
+                                // Ignore the exception as it does not affect the overall authentication flow
+                                logger.Warn("External browser response not sent out");
+                            }
                         }
                     }
                 }
