ef4ec890ef546804199757579626f0c9c4e801be
Dawid Heyman <dawid.heyman@snowflake.com>
2024-07-09 10:03:52 +0200

    CR suggestions applied

--- Snowflake.Data.Tests/IntegrationTests/SFMaxLobSizeSwitchIT.cs
+++ Snowflake.Data.Tests/IntegrationTests/SFMaxLobSizeSwitchIT.cs
@@ -7,44 +7,30 @@ namespace Snowflake.Data.Tests.IntegrationTests
     [TestFixture]
     public class SFMaxLobSizeSwitchIT : SFBaseTest
     {
+        private const string SqlSelectLargeString = "select randstr(20000000, random()) as large_str";
+
         [Test]
         [Ignore("TODO: Enable when Max LOB size is available on the automated tests environment")]
         public void TestIncreaseMaxLobSizeParameterSwitchSelect()
         {
-            using (var conn = new SnowflakeDbConnection(ConnectionString))
+            using (var conn = new SnowflakeDbConnection(ConnectionString + "poolingEnabled=false"))
             {
                 conn.Open();
                 IDbCommand cmd = conn.CreateCommand();
-
-                //setup
                 cmd.CommandText = "alter session set FEATURE_INCREASED_MAX_LOB_SIZE_IN_MEMORY='ENABLED'";
                 cmd.ExecuteNonQuery();
                 cmd.CommandText = "alter session set ENABLE_LARGE_VARCHAR_AND_BINARY_IN_RESULT=false";
                 cmd.ExecuteNonQuery();
 
-                //initial test
-                try
-                {
-                    cmd.CommandText = "select randstr(20000000, random()) as large_str";
-                    cmd.ExecuteReader();
-                }
-                catch (SnowflakeDbException e)
-                {
-                    Assert.That(e.Message, Does.Contain("exceeds supported length"));
-                }
+                cmd.CommandText = SqlSelectLargeString;
+                var thrown = Assert.Throws<SnowflakeDbException>(() => cmd.ExecuteReader());
+                Assert.That(thrown.Message, Does.Contain("exceeds supported length"));
 
-                //parameter switch
                 cmd.CommandText = "alter session set ENABLE_LARGE_VARCHAR_AND_BINARY_IN_RESULT=true";
                 cmd.ExecuteNonQuery();
-                cmd.CommandText = "select randstr(20000000, random()) as large_str";
+                cmd.CommandText = SqlSelectLargeString;
                 var reader = cmd.ExecuteReader();
                 Assert.IsTrue(reader.Read());
-
-                //cleanup
-                cmd.CommandText = "alter session unset ENABLE_LARGE_VARCHAR_AND_BINARY_IN_RESULT";
-                cmd.ExecuteNonQuery();
-                cmd.CommandText = "alter session unset ENABLE_LARGE_VARCHAR_AND_BINARY_IN_RESULT";
-                cmd.ExecuteNonQuery();
             }
         }
     }
