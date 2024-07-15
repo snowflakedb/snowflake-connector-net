09325e89bd95449b5129e1a6d192d822af60eeee
Juan Martinez Ramirez <juan.martinezramirez@snowflake.com>
2024-07-15 07:44:49 -0600

    Changed cloud provider to AZURE

--- Snowflake.Data.Tests/UnitTests/SFRemoteStorageClientTest.cs
+++ Snowflake.Data.Tests/UnitTests/SFRemoteStorageClientTest.cs
@@ -19,6 +19,8 @@ namespace Snowflake.Data.Tests.UnitTests
     using Moq;
 
     [TestFixture]
+    [IgnoreOnEnvIs("snowflake_cloud_env",
+        new string[] { "AZURE", "AWS" })]
     class SFRemoteStorageClientTest : SFBaseTest
     {
         // Mock data for file metadata
