907618206910aade290532ac1f290b0496dc6c01
Angel Avalos <angel.avalos@snowflake.com>
2024-07-30 13:38:11 -0700

    testing cosign

--- deploy.bat
+++ deploy.bat
@@ -12,10 +12,10 @@ echo -----END CERTIFICATE----- >> %WORKSPACE%\coded.txt
 
 certutil -decode %WORKSPACE%\coded.txt %WORKSPACE%\key.snk
 
-SET VERSION=4.0.0
+SET VERSION=3.0.0
 
-dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%WORKSPACE%\key.snk
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v %WORKSPACE%"\Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
+@REM dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%WORKSPACE%\key.snk
+@REM "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v %WORKSPACE%"\Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
 
 dotnet pack Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n --no-build  --output %ROOT_DIR%
 
