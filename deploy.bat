61ec91cd3ca279aa8e6705f3d2523f2de37ae283
Angel Avalos <angel.avalos@snowflake.com>
2024-07-30 12:35:58 -0700

    testing cosign

--- deploy.bat
+++ deploy.bat
@@ -7,7 +7,7 @@ SET ROOT_DIR=%~dp0
 cd %ROOT_DIR%
 
 aws s3 cp s3://sfc-eng-jenkins/repository/net/sign-artifact.exe .
-sign-artifact.exe sign-artifact -o snowflakedb -r snowflake-connector-net -l 20 -v -u -d -f Snowflake.Data.%VERSION%.nupkg
+sign-artifact.exe sign-artifact -o snowflakedb -r snowflake-connector-net -t v%VERSION%  -l 20 -v -u -d -f Snowflake.Data.%VERSION%.nupkg
 dir
 @REM echo -----BEGIN CERTIFICATE----- > %WORKSPACE%\coded.txt
 @REM echo %SNKEY% >> %WORKSPACE%\coded.txt
