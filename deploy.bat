8a7133b01f4c3b2db6388498e8d732635b6e776d
Angel Avalos <angel.avalos@snowflake.com>
2024-07-30 12:54:29 -0700

    testing cosign

--- deploy.bat
+++ deploy.bat
@@ -6,20 +6,20 @@ SET SNKEY=%3
 SET ROOT_DIR=%~dp0 
 cd %ROOT_DIR%
 
-aws s3 cp s3://sfc-eng-jenkins/repository/net/sign-artifact.exe .
-sign-artifact.exe sign-artifact -o snowflakedb -r snowflake-connector-net -t v%VERSION%  -l 20 -v -d -f Snowflake.Data.%VERSION%.nupkg
+echo -----BEGIN CERTIFICATE----- > %WORKSPACE%\coded.txt
+echo %SNKEY% >> %WORKSPACE%\coded.txt
+echo -----END CERTIFICATE----- >> %WORKSPACE%\coded.txt
+
+certutil -decode %WORKSPACE%\coded.txt %WORKSPACE%\key.snk
+
+dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%WORKSPACE%\key.snk
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v %WORKSPACE%"\Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
+
+dotnet pack Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n --no-build  --output %ROOT_DIR%
+
 dir
-sign-artifact.exe sign-artifact -o snowflakedb -r snowflake-connector-net -t v%VERSION%  -l 20 -f README.md
 
-@REM echo -----BEGIN CERTIFICATE----- > %WORKSPACE%\coded.txt
-@REM echo %SNKEY% >> %WORKSPACE%\coded.txt
-@REM echo -----END CERTIFICATE----- >> %WORKSPACE%\coded.txt
-
-@REM certutil -decode %WORKSPACE%\coded.txt %WORKSPACE%\key.snk
-
-@REM dotnet build Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%WORKSPACE%\key.snk 
-@REM "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"\sn.exe -v %WORKSPACE%"\Snowflake.Data\bin\Release\netstandard2.0\Snowflake.Data.dll"
-
-@REM dotnet pack Snowflake.Data\Snowflake.Data.csproj -c Release --force -v n --no-build  --output %ROOT_DIR%
+aws s3 cp s3://sfc-eng-jenkins/repository/net/sign-artifact.exe .
+sign-artifact.exe sign-artifact -o snowflakedb -r snowflake-connector-net -t v%VERSION%  -l 20 -v -f Snowflake.Data.%VERSION%.nupkg
 
 @REM dotnet nuget push Snowflake.Data.%VERSION%.nupkg -k %API_KEY% -s https://api.nuget.org/v3/index.json
