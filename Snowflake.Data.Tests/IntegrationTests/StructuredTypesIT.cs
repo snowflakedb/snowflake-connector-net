9ce3aad47396f423753a7668f8b0c46dac35828c
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2024-07-31 15:41:39 +0000

    try to enable structured types

--- Snowflake.Data.Tests/IntegrationTests/StructuredTypesIT.cs
+++ Snowflake.Data.Tests/IntegrationTests/StructuredTypesIT.cs
@@ -9,11 +9,11 @@ namespace Snowflake.Data.Tests.IntegrationTests
         {
             using (var command = connection.CreateCommand())
             {
-                command.CommandText = "alter session set ENABLE_STRUCTURED_TYPES_IN_CLIENT_RESPONSE=true";
+                command.CommandText = "alter session set ENABLE_STRUCTURED_TYPES_IN_CLIENT_RESPONSE = true";
                 command.ExecuteNonQuery();
-                command.CommandText = "ALTER SESSION SET FEATURE_STRUCTURED_TYPES = enabled";
+                command.CommandText = "lter session set IGNORE_CLIENT_VESRION_IN_STRUCTURED_TYPES_RESPONSE = true";
                 command.ExecuteNonQuery();
-                command.CommandText = "ALTER SESSION SET DOTNET_QUERY_RESULT_FORMAT=JSON";
+                command.CommandText = "ALTER SESSION SET DOTNET_QUERY_RESULT_FORMAT = JSON";
                 command.ExecuteNonQuery();
             }
         }
