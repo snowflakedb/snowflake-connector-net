4e35ac97ad7a49be431828c0c8c31eb9e72588f6
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2024-09-02 14:05:44 +0000

    Add comment to StreamPair class

--- Snowflake.Data/Core/FileTransfer/StreamPair.cs
+++ Snowflake.Data/Core/FileTransfer/StreamPair.cs
@@ -3,6 +3,13 @@ using System.IO;
 
 namespace Snowflake.Data.Core.FileTransfer
 {
+    /*
+     * StreamPair class has been introduced to solve the issue for a stream which is meant to be returned from a method,
+     * but another helper stream is created in this method and is tightly coupled with the main stream,
+     * so the helper stream cannot be closed in this method because it would close the main stream as well
+     * (if CryptoStream in EncryptionProvider class would be disposed it would close the base stream as well).
+     * The solution is to return both streams and dispose both of them together when processing of the main stream is over.
+     */
     internal class StreamPair: IDisposable
     {
         public Stream MainStream { get; set; }
