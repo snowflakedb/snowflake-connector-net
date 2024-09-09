cd5b9bb943ca37fc69d09738785a6f08650af2a6
Louie Farol <louie.farol@insightsoftware.com>
2024-09-09 14:47:55 -0700

    Add pragma warning disable

--- Snowflake.Data.Tests/Mock/MockInfiniteTimeout.cs
+++ Snowflake.Data.Tests/Mock/MockInfiniteTimeout.cs
@@ -37,7 +37,11 @@ namespace Snowflake.Data.Tests.Mock
                                                               CancellationToken externalCancellationToken,
                                                               string sid = "")
         {
+            // Disable warning as this is the way to be compliant with netstandard2.0
+            // API reference: https://learn.microsoft.com/en-us/dotnet/api/system.net.http.httprequestmessage?view=netstandard-2.0
+#pragma warning disable CS0618 // Type or member is obsolete
             message.Properties[BaseRestRequest.HTTP_REQUEST_TIMEOUT_KEY] = Timeout.InfiniteTimeSpan;
+#pragma warning restore CS0618 // Type or member is obsolete
             return await (base.SendAsync(message, restTimeout, externalCancellationToken).ConfigureAwait(false));
         }
     }
