a90deca0138a839dc955cceb4b3f944fda9679d9
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-01-28 10:48:25 +0100

    only one test

--- Snowflake.Data.Tests/AuthenticationTests/KeyPairConnection.cs
+++ Snowflake.Data.Tests/AuthenticationTests/KeyPairConnection.cs
@@ -2,6 +2,7 @@
  * Copyright (c) 2012-2025 Snowflake Computing Inc. All rights reserved.
  */
 
+using System;
 using NUnit.Framework;
 
 
@@ -16,55 +17,55 @@ namespace Snowflake.Data.Tests.AuthenticationTests
 
 
         [Test, IgnoreOnCI]
-        public void TestAuthenticateUsingKeyPairFilcContentSuccessful()
+        public void TestAuthenticateUsingKeyPairFileContentSuccessful()
         {
             AuthTestHelper authTestHelper = new AuthTestHelper();
 
             var privateKey = AuthConnectionString.GetPrivateKeyContentForKeypairAuth("SNOWFLAKE_AUTH_TEST_PRIVATE_KEY_PATH");
             var parameters = AuthConnectionString.GetKeyPairFromFileContentParameters(privateKey);
             _connectionString = AuthConnectionString.SetPrivateKeyFromFileContentConnectionString(parameters);
-
+            Console.WriteLine(_connectionString);
             authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
             authTestHelper.VerifyExceptionIsNotThrown();
         }
-
-        [Test, IgnoreOnCI]
-        public void TestAuthenticateUsingKeyPairFileContentInvalidKey()
-        {
-            AuthTestHelper authTestHelper = new AuthTestHelper();
-
-            var privateKey = AuthConnectionString.GetPrivateKeyContentForKeypairAuth("SNOWFLAKE_AUTH_TEST_INVALID_PRIVATE_KEY_PATH");
-            var parameters = AuthConnectionString.GetKeyPairFromFileContentParameters(privateKey);
-            _connectionString = AuthConnectionString.SetPrivateKeyFromFileContentConnectionString(parameters);
-
-            authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
-            authTestHelper.VerifyExceptionIsThrown("Error: JWT token is invalid");
-        }
-
-         [Test, IgnoreOnCI]
-         public void TestAuthenticateUsingKeyPairFilePathSuccessful()
-         {
-             AuthTestHelper authTestHelper = new AuthTestHelper();
-
-             var privateKeyPath = AuthConnectionString.GetPrivateKeyPathForKeypairAuth("SNOWFLAKE_AUTH_TEST_PRIVATE_KEY_PATH");
-             var parameters = AuthConnectionString.GetKeyPairFromFilePathConnectionString(privateKeyPath);
-             _connectionString = AuthConnectionString.SetPrivateKeyFromFilePathConnectionString(parameters);
-
-             authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
-             authTestHelper.VerifyExceptionIsNotThrown();
-         }
-
-        [Test, IgnoreOnCI]
-        public void TestAuthenticateUsingKeyPairFilePathInvalidKey()
-        {
-            AuthTestHelper authTestHelper = new AuthTestHelper();
-
-            var privateKeyPath = AuthConnectionString.GetPrivateKeyPathForKeypairAuth("SNOWFLAKE_AUTH_TEST_INVALID_PRIVATE_KEY_PATH");
-            var parameters = AuthConnectionString.GetKeyPairFromFilePathConnectionString(privateKeyPath);
-            _connectionString = AuthConnectionString.SetPrivateKeyFromFilePathConnectionString(parameters);
-
-            authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
-            authTestHelper.VerifyExceptionIsThrown("Error: JWT token is invalid");
-        }
-    }
+//         //
+//         // [Test, IgnoreOnCI]
+//         // public void TestAuthenticateUsingKeyPairFileContentInvalidKey()
+//         // {
+//         //     AuthTestHelper authTestHelper = new AuthTestHelper();
+//         //
+//         //     var privateKey = AuthConnectionString.GetPrivateKeyContentForKeypairAuth("SNOWFLAKE_AUTH_TEST_INVALID_PRIVATE_KEY_PATH");
+//         //     var parameters = AuthConnectionString.GetKeyPairFromFileContentParameters(privateKey);
+//         //     _connectionString = AuthConnectionString.SetPrivateKeyFromFileContentConnectionString(parameters);
+//         //
+//         //     authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
+//         //     authTestHelper.VerifyExceptionIsThrown("Error: JWT token is invalid");
+//         // }
+//         //
+//         //  [Test, IgnoreOnCI]
+//         //  public void TestAuthenticateUsingKeyPairFilePathSuccessful()
+//         //  {
+//         //      AuthTestHelper authTestHelper = new AuthTestHelper();
+//         //
+//         //      var privateKeyPath = AuthConnectionString.GetPrivateKeyPathForKeypairAuth("SNOWFLAKE_AUTH_TEST_PRIVATE_KEY_PATH");
+//         //      var parameters = AuthConnectionString.GetKeyPairFromFilePathConnectionString(privateKeyPath);
+//         //      _connectionString = AuthConnectionString.SetPrivateKeyFromFilePathConnectionString(parameters);
+//         //
+//         //      authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
+//         //      authTestHelper.VerifyExceptionIsNotThrown();
+//         //  }
+//         //
+//         // [Test, IgnoreOnCI]
+//         // public void TestAuthenticateUsingKeyPairFilePathInvalidKey()
+//         // {
+//         //     AuthTestHelper authTestHelper = new AuthTestHelper();
+//         //
+//         //     var privateKeyPath = AuthConnectionString.GetPrivateKeyPathForKeypairAuth("SNOWFLAKE_AUTH_TEST_INVALID_PRIVATE_KEY_PATH");
+//         //     var parameters = AuthConnectionString.GetKeyPairFromFilePathConnectionString(privateKeyPath);
+//         //     _connectionString = AuthConnectionString.SetPrivateKeyFromFilePathConnectionString(parameters);
+//         //
+//         //     authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
+//         //     authTestHelper.VerifyExceptionIsThrown("Error: JWT token is invalid");
+//         // }
+     }
 }
--- Snowflake.Data.Tests/AuthenticationTests/OauthConnection.cs
+++ Snowflake.Data.Tests/AuthenticationTests/OauthConnection.cs
@@ -1,62 +1,62 @@
-/*
- * Copyright (c) 2012-2025 Snowflake Computing Inc. All rights reserved.
- */
-
-using NUnit.Framework;
-using Snowflake.Data.Core;
-
-namespace Snowflake.Data.Tests.AuthenticationTests
-{
-
-    [NonParallelizable]
-    public class OauthConnectionTest : SFBaseTest
-    {
-        private string _connectionString = "";
-
-        [SetUp]
-        public void SetUp()
-        {
-            string token = AuthConnectionString.GetOauthToken();
-            var parameters = AuthConnectionString.GetOauthConnectionString(token);
-            _connectionString = AuthConnectionString.SetOauthConnectionString(parameters);
-
-        }
-
-        [Test, IgnoreOnCI]
-        public void TestAuthenticateUsingOauthSuccessful()
-        {
-            AuthTestHelper authTestHelper = new AuthTestHelper();
-
-            authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
-            authTestHelper.VerifyExceptionIsNotThrown();
-        }
-
-        [Test, IgnoreOnCI]
-        public void TestAuthenticateUsingOauthInvalidToken()
-        {
-            AuthTestHelper authTestHelper = new AuthTestHelper();
-
-            string token = "invalidToken";
-            var parameters = AuthConnectionString.GetOauthConnectionString(token);
-            _connectionString = AuthConnectionString.SetOauthConnectionString(parameters);
-
-            authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
-            authTestHelper.VerifyExceptionIsThrown("Invalid OAuth access token");
-        }
-
-        // [Test, Ignore("Skipped, waits for SNOW-1893041")]
-        // public void TestAuthenticateUsingOauthMismatchedUser()
-        // {
-        //     AuthTestHelper authTestHelper = new AuthTestHelper();
-        //
-        //     string token = AuthConnectionString.GetOauthToken();
-        //     var parameters = AuthConnectionString.GetOauthConnectionString(token);
-        //     parameters[SFSessionProperty.USER] = "fakeAccount";
-        //     _connectionString = AuthConnectionString.SetOauthConnectionString(parameters) + ";poolingEnabled=false;minPoolSize=0;";
-        //
-        //     authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
-        //     authTestHelper.VerifyExceptionIsThrown("The user you were trying to authenticate as differs from the user tied to the access token");
-        //
-        // }
-    }
-}
+// /*
+//  * Copyright (c) 2012-2025 Snowflake Computing Inc. All rights reserved.
+//  */
+//
+// using NUnit.Framework;
+// using Snowflake.Data.Core;
+//
+// namespace Snowflake.Data.Tests.AuthenticationTests
+// {
+//
+//     [NonParallelizable]
+//     public class OauthConnectionTest : SFBaseTest
+//     {
+//         private string _connectionString = "";
+//
+//         [SetUp]
+//         public void SetUp()
+//         {
+//             string token = AuthConnectionString.GetOauthToken();
+//             var parameters = AuthConnectionString.GetOauthConnectionString(token);
+//             _connectionString = AuthConnectionString.SetOauthConnectionString(parameters);
+//
+//         }
+//
+//         // [Test, IgnoreOnCI]
+//         // public void TestAuthenticateUsingOauthSuccessful()
+//         // {
+//         //     AuthTestHelper authTestHelper = new AuthTestHelper();
+//         //
+//         //     authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
+//         //     authTestHelper.VerifyExceptionIsNotThrown();
+//         // }
+//         //
+//         // [Test, IgnoreOnCI]
+//         // public void TestAuthenticateUsingOauthInvalidToken()
+//         // {
+//         //     AuthTestHelper authTestHelper = new AuthTestHelper();
+//         //
+//         //     string token = "invalidToken";
+//         //     var parameters = AuthConnectionString.GetOauthConnectionString(token);
+//         //     _connectionString = AuthConnectionString.SetOauthConnectionString(parameters);
+//         //
+//         //     authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
+//         //     authTestHelper.VerifyExceptionIsThrown("Invalid OAuth access token");
+//         // }
+//
+//         // [Test, Ignore("Skipped, waits for SNOW-1893041")]
+//         // public void TestAuthenticateUsingOauthMismatchedUser()
+//         // {
+//         //     AuthTestHelper authTestHelper = new AuthTestHelper();
+//         //
+//         //     string token = AuthConnectionString.GetOauthToken();
+//         //     var parameters = AuthConnectionString.GetOauthConnectionString(token);
+//         //     parameters[SFSessionProperty.USER] = "fakeAccount";
+//         //     _connectionString = AuthConnectionString.SetOauthConnectionString(parameters) + ";poolingEnabled=false;minPoolSize=0;";
+//         //
+//         //     authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
+//         //     authTestHelper.VerifyExceptionIsThrown("The user you were trying to authenticate as differs from the user tied to the access token");
+//         //
+//         // }
+//     }
+// }
