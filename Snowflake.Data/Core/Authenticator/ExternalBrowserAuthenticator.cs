aa237d5132b7d196fc60b1257f4fef53c1fc0c91
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2025-01-28 16:52:52 +0100

    SNOW-715504 Fix secondary authentication data in authenticators

--- Snowflake.Data/Core/Authenticator/ExternalBrowserAuthenticator.cs
+++ Snowflake.Data/Core/Authenticator/ExternalBrowserAuthenticator.cs
@@ -260,7 +260,7 @@ namespace Snowflake.Data.Core.Authenticator
             // Add the token and proof key to the Data
             data.Token = _samlResponseToken;
             data.ProofKey = _proofKey;
-            SetSpecializedAuthenticatorData(ref data);
+            SetSecondaryAuthenticationData(ref data);
         }
 
         private string GetLoginUrl(string proofKey, int localPort)
