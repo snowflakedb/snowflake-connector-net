bee0b3fdfb63a7a326138da7d01758f53fb31cfe
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-01-29 09:49:35 +0100

    without parameters.json in docker

--- ci/container/test_authentication.sh
+++ ci/container/test_authentication.sh
@@ -12,14 +12,4 @@ eval $(jq -r '.authtestparams | to_entries | map("export \(.key)=\(.value|tostri
 export SNOWFLAKE_AUTH_TEST_PRIVATE_KEY_PATH=./.github/workflows/parameters/rsa_keys/rsa_key.p8
 export SNOWFLAKE_AUTH_TEST_INVALID_PRIVATE_KEY_PATH=./.github/workflows/parameters/rsa_keys/rsa_key_invalid.p8
 
-PARAMETERS_JSON="./Snowflake.Data.Tests/parameters.json"
-echo '{ "testconnection": { } }' > $PARAMETERS_JSON
-
-#env | grep '^SNOWFLAKE_TEST' | while IFS='=' read -r key value; do
-#  echo "  \"$key\": \"$value\"," >> $PARAMETERS_JSON
-#done
-## Remove the last comma and close the JSON object
-#awk '{if (NR>1) print prev; prev=$0} END {sub(/,$/, "", prev); print prev}' $PARAMETERS_JSON > tmp && mv tmp $PARAMETERS_JSON
-#echo '}}' >> $PARAMETERS_JSON
-
 dotnet test --framework net9.0 -l "console;verbosity=diagnostic" --filter FullyQualifiedName~AuthenticationTests
