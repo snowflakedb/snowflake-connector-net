95db86263dc249d062b7c70d063d6559720187a2
Louie Farol <louie.farol@insightsoftware.com>
2025-02-06 14:08:28 -0800

    Fix year

--- Snowflake.Data/Logger/ILoggerEmptyImpl.cs
+++ Snowflake.Data/Logger/ILoggerEmptyImpl.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2019 Snowflake Computing Inc. All rights reserved.
+ * Copyright (c) 2012-2025 Snowflake Computing Inc. All rights reserved.
  */
 
 using Microsoft.Extensions.Logging;
