1e0c501fb6405e3ec4b0162e49512d8e0a8af1a4
Louie Farol <louie.farol@insightsoftware.com>
2025-02-07 10:09:45 -0800

    Remove unused package

--- Snowflake.Data/Configuration/SFConfigurationSectionHandler.cs
+++ Snowflake.Data/Configuration/SFConfigurationSectionHandler.cs
@@ -2,29 +2,18 @@
  * Copyright (c) 2012-2019 Snowflake Computing Inc. All rights reserved.
  */
 
-using System.Collections;
-using System.Configuration;
-using System.Xml;
-
 namespace Snowflake.Data.Configuration
 {
     /// <summary>
     ///     Develop In progress
     /// </summary>
-    public class SFConfigurationSectionHandler : IConfigurationSectionHandler
+    public class SFConfigurationSectionHandler
     {
         static SFConfigurationSectionHandler()
         { }
 
         public SFConfigurationSectionHandler()
         { }
-
-        #region IConfigurationSectionHandler Memebers
-        object IConfigurationSectionHandler.Create(object parent, object configContext, XmlNode section)
-        {
-            return null;
-        }
-        #endregion
     }
 
 }
