5fa96f6c5b468c157bf7ec45bdb95684782fe95f
sfc-gh-ext-simba-lf <louie.farol@insightsoftware.com>
2025-03-01 01:56:38 -0800

    Drop copyright header in comments

--- Snowflake.Data.Tests/Mock/MockS3Client.cs
+++ Snowflake.Data.Tests/Mock/MockS3Client.cs
@@ -1,7 +1,3 @@
-﻿/*
- * Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
- */
-
 using Amazon.S3;
 using Amazon.S3.Model;
 using Snowflake.Data.Core.FileTransfer.StorageClient;
--- Snowflake.Data/Core/QueryResultsAwaiter.cs
+++ Snowflake.Data/Core/QueryResultsAwaiter.cs
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2024 Snowflake Computing Inc. All rights reserved.
- */
-
 using Snowflake.Data.Log;
 using System.Threading.Tasks;
 using System.Threading;
