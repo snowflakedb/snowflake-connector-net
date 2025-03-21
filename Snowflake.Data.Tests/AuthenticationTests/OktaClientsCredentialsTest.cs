533be8c7e14524cc505184701affe3a15dd6d452
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-03-21 13:14:53 +0100

    init

---
+++ Snowflake.Data.Tests/AuthenticationTests/OktaClientsCredentialsTest.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using Snowflake.Data.Tests;
+
+namespace Snowflake.Data.AuthenticationTests
+{
+    [NonParallelizable, IgnoreOnCI]
+    public class OktaClientsCredentialsTest
+    {
+        private string _connectionString = "";
+
+        [Test, IgnoreOnCI]
+        public void TestAuthenticateOktaClientCredentialsSuccessful()
+
+        {
+
+        }
+
+
+        [Test, IgnoreOnCI]
+        public void TestAuthenticateOktaClientCredentialsMismatchedUsername()
+
+        {
+
+        }
+
+
+        [Test, IgnoreOnCI]
+        public void TestAuthenticateOktaClientCredentialsUnauthorized()
+
+        {
+
+        }
+
+
+
+    }
+}
