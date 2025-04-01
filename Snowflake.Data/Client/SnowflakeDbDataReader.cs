933d2266713c907513e352f338b62ac08a463b16
sfc-gh-ext-simba-lf <louie.farol@insightsoftware.com>
2025-04-01 13:54:06 -0700

    Revert fix

--- Snowflake.Data/Client/SnowflakeDbDataReader.cs
+++ Snowflake.Data/Client/SnowflakeDbDataReader.cs
@@ -118,7 +118,8 @@ namespace Snowflake.Data.Client
 
                 row[SchemaTableColumn.ColumnName] = rowType.name;
                 row[SchemaTableColumn.ColumnOrdinal] = columnOrdinal;
-                row[SchemaTableColumn.ColumnSize] = IsSemiStructuredType(rowType.type) && rowType.length == 0 ? -1 : (int)rowType.length;
+                //row[SchemaTableColumn.ColumnSize] = IsSemiStructuredType(rowType.type) && rowType.length == 0 ? -1 : (int)rowType.length;
+                row[SchemaTableColumn.ColumnSize] = (int)rowType.length;
                 row[SchemaTableColumn.NumericPrecision] = (int)rowType.precision;
                 row[SchemaTableColumn.NumericScale] = (int)rowType.scale;
                 row[SchemaTableColumn.AllowDBNull] = rowType.nullable;
