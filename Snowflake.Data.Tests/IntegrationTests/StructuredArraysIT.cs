1ddc445f08648a1e1feb8c85f1f8f62f7c7600bc
sfc-gh-ext-simba-lf <louie.farol@insightsoftware.com>
2025-04-01 12:29:20 -0700

    Test

--- Snowflake.Data.Tests/IntegrationTests/StructuredArraysIT.cs
+++ Snowflake.Data.Tests/IntegrationTests/StructuredArraysIT.cs
@@ -19,6 +19,8 @@ namespace Snowflake.Data.Tests.IntegrationTests
             // arrange
             using (var connection = new SnowflakeDbConnection(ConnectionString))
             {
+                connection.Open();
+
                 var colName = "c1";
                 CreateOrReplaceTable(connection, TableName, new[] { $"{colName} ARRAY" });
 
