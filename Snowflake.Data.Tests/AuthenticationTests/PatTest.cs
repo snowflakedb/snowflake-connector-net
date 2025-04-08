85a3d3b7a030b44f76972ed9d7f7e788df488099
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-04-08 16:26:44 +0200

    review1

--- Snowflake.Data.Tests/AuthenticationTests/PatTest.cs
+++ Snowflake.Data.Tests/AuthenticationTests/PatTest.cs
@@ -24,10 +24,16 @@ namespace Snowflake.Data.AuthenticationTests
         {
             AuthTestHelper authTestHelper = new AuthTestHelper();
             var parameters = AuthConnectionString.GetPatConnectionParameters();
-            parameters[SFSessionProperty.TOKEN] = GetPat();
-            _connectionString = AuthConnectionString.ConvertToConnectionString(parameters);
-            authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
-            RemovePat();
+            try
+            {
+                parameters[SFSessionProperty.TOKEN] = GetPat();
+                _connectionString = AuthConnectionString.ConvertToConnectionString(parameters);
+                authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
+            }
+            finally
+            {
+                RemovePat();
+            }
             authTestHelper.VerifyExceptionIsNotThrown();
 
         }
@@ -49,11 +55,17 @@ namespace Snowflake.Data.AuthenticationTests
             AuthTestHelper authTestHelper = new AuthTestHelper();
             authTestHelper.VerifyExceptionIsNotThrown();
             var parameters = AuthConnectionString.GetPatConnectionParameters();
-            parameters[SFSessionProperty.TOKEN] = GetPat();
-            parameters[SFSessionProperty.USER] = "differentUser";
-            _connectionString = AuthConnectionString.ConvertToConnectionString(parameters);
-            authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
-            RemovePat();
+            try
+            {
+                parameters[SFSessionProperty.TOKEN] = GetPat();
+                parameters[SFSessionProperty.USER] = "differentUser";
+                _connectionString = AuthConnectionString.ConvertToConnectionString(parameters);
+                authTestHelper.ConnectAndExecuteSimpleQuery(_connectionString);
+            }
+            finally
+            {
+                RemovePat();
+            }
             authTestHelper.VerifyExceptionIsThrown("Programmatic access token is invalid.");
         }
 
@@ -63,7 +75,7 @@ namespace Snowflake.Data.AuthenticationTests
             _patName = "PAT_DOTNET_" + GenerateRandomSuffix();
             var parameters = AuthConnectionString.GetPatConnectionParameters();
             string command = string.Format(
-                "alter user {0} add programmatic access token {1} ROLE_RESTRICTION = '{2}';",
+                "alter user {0} add programmatic access token {1} ROLE_RESTRICTION = '{2}' DAYS_TO_EXPIRY=1;",
                 AuthConnectionString.SnowflakeUser, _patName, AuthConnectionString.SnowflakeRole);
             var patToken = authTestHelper.ConnectUsingOktaConnectionAndExecuteCustomCommand(command, true);
             return patToken;
