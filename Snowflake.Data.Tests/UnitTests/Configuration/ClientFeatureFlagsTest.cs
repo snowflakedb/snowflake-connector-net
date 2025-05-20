1ea0e0ff7d4a157aef6926eaa58afce98b7ca5b2
Piotr Bulawa <piotr.bulawa@snowflake.com>
2025-05-20 09:39:34 +0200

    Format

--- Snowflake.Data.Tests/UnitTests/Configuration/ClientFeatureFlagsTest.cs
+++ Snowflake.Data.Tests/UnitTests/Configuration/ClientFeatureFlagsTest.cs
@@ -17,7 +17,8 @@ namespace Snowflake.Data.Tests.UnitTests.Configuration
         [TestCase(ClientFeatureFlags.EnabledExperimentalAuthenticationVariableName, null, false)]
         [TestCase(ClientFeatureFlags.EnabledExperimentalAuthenticationVariableName, "not a bool value", false)]
         [TestCase("OTHER_VARIABLE_NAME", "true", false)]
-        public void TestEnabledExperimentalAuthentication(string variableName, string variableValue, bool expectedValue) {
+        public void TestEnabledExperimentalAuthentication(string variableName, string variableValue, bool expectedValue)
+        {
             // arrange
             var environmentOperations = new Mock<EnvironmentOperations>();
             environmentOperations
