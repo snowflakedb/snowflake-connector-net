0d1ae8b4d664ee3f11c9a1dec7f5047955411b94
sfc-gh-ext-simba-lf <louie.farol@insightsoftware.com>
2025-05-22 13:29:04 -0700

    Fix whitespace

--- Snowflake.Data/Logger/SFRollingFileAppender.cs
+++ Snowflake.Data/Logger/SFRollingFileAppender.cs
@@ -37,7 +37,7 @@ internal class SFRollingFileAppender : SFAppender
             else
             {
                 var fileInfo = new UnixFileInfo(path: LogFilePath);
-                using (var handle = fileInfo.Open(FileMode.Append, FileAccess.ReadWrite, FilePermissions.S_IWUSR |  FilePermissions.S_IRUSR))
+                using (var handle = fileInfo.Open(FileMode.Append, FileAccess.ReadWrite, FilePermissions.S_IWUSR | FilePermissions.S_IRUSR))
                 {
                     SFCredentialManagerFileImpl.Instance.ValidateFilePermissions(handle);
                     UnixOperations.Instance.WriteAllText(handle, formattedMessage, null);
