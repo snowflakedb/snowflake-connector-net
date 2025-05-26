703ef6122f8101e22ba1dfb9def5bf4ed7d5585d
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2025-05-26 10:58:34 +0200

    Fix log message

--- Snowflake.Data/Core/RestRequester.cs
+++ Snowflake.Data/Core/RestRequester.cs
@@ -134,7 +134,7 @@ namespace Snowflake.Data.Core
                         if (watch.IsRunning)
                         {
                             watch.Stop();
-                            logger.Error($"Response receiving interrupted by exception after {watch.ElapsedMilliseconds} ms");
+                            logger.Error($"Response receiving interrupted by exception after {watch.ElapsedMilliseconds} ms. {sid} {message.Method} {message.RequestUri}");
                         }
                         // Disposing of the response if not null now that we don't need it anymore
                         response?.Dispose();
