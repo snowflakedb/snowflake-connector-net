2871eacf90a634330db0a076ef7ca5abc0412846
Adam Kolodziejczyk <adam.kolodziejczyk@snowflake.com>
2025-07-29 13:02:12 +0200

    remove not needed envs

--- ci/wif/test_wif.sh
+++ ci/wif/test_wif.sh
@@ -4,6 +4,4 @@ set -o pipefail
 
 export SF_ENABLE_EXPERIMENTAL_AUTHENTICATION=true
 
-SNOWFLAKE_TEST_WIF_ACCOUNT="$SNOWFLAKE_TEST_WIF_ACCOUNT" \
-SNOWFLAKE_TEST_WIF_HOST="$SNOWFLAKE_TEST_WIF_HOST" \
 dotnet test --framework net9.0 -l "console;verbosity=info" --filter FullyQualifiedName~WIFTests
