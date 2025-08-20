a33320b5c8893c673f3f939a127e1952ebc3da6a
sfc-gh-ext-simba-lf <louie.farol@insightsoftware.com>
2025-08-19 18:43:22 -0700

    Add tests for new connection limit property

--- Snowflake.Data.Tests/UnitTests/Session/SFHttpClientPropertiesTest.cs
+++ Snowflake.Data.Tests/UnitTests/Session/SFHttpClientPropertiesTest.cs
@@ -51,6 +51,37 @@ namespace Snowflake.Data.Tests.UnitTests.Session
         }
 
         [Test]
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        public void TestSettingConnectionLimitProperty(int expectedConnectionLimit)
+        {
+            // arrange
+            var connectionString = $"ACCOUNT=account;USER=test;PASSWORD=test;CONNECTION_LIMIT={expectedConnectionLimit}";
+            var properties = SFSessionProperties.ParseConnectionString(connectionString, new SessionPropertiesContext());
+
+            // act
+            var extractedProperties = SFSessionHttpClientProperties.ExtractAndValidate(properties);
+
+            // assert
+            Assert.AreEqual(expectedConnectionLimit, extractedProperties._connectionLimit);
+        }
+
+        [Test]
+        public void TestSettingConnectionLimitPropertyToLessThan1()
+        {
+            // arrange
+            var connectionString = $"ACCOUNT=account;USER=test;PASSWORD=test;CONNECTION_LIMIT={0}";
+            var properties = SFSessionProperties.ParseConnectionString(connectionString, new SessionPropertiesContext());
+
+            // act
+            var extractedProperties = SFSessionHttpClientProperties.ExtractAndValidate(properties);
+
+            // assert
+            Assert.AreEqual(SFSessionHttpClientProperties.DefaultConnectionLimit, extractedProperties._connectionLimit);
+        }
+
+        [Test]
         public void TestBuildHttpClientConfig()
         {
             // arrange
