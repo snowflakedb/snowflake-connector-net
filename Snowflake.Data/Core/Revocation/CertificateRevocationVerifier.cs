f1e87e44851dcc25999d998389e9a5a2e1552479
Krzysztof Nozderko <krzysztof.nozderko@snowflake.com>
2025-09-01 16:10:01 +0200

    Revert "handle unexpected exceptions"
    This reverts commit 4814f9453daa6f60e7bf64a4b926e60766fc62a3.

--- Snowflake.Data/Core/Revocation/CertificateRevocationVerifier.cs
+++ Snowflake.Data/Core/Revocation/CertificateRevocationVerifier.cs
@@ -48,15 +48,7 @@ namespace Snowflake.Data.Core.Revocation
             bool defaultValidationPassed = sslPolicyErrors == SslPolicyErrors.None;
             if (!defaultValidationPassed)
                 return false;
-            try
-            {
-                return CheckCertificateRevocationStatus(certificate, chain);
-            }
-            catch (Exception exception)
-            {
-                s_logger.Error($"Unexpected exception checking certificate revocation status: {exception.Message}", exception);
-                return false;
-            }
+            return CheckCertificateRevocationStatus(certificate, chain);
         }
 
         private bool CheckCertificateRevocationStatus(X509Certificate2 certificate, X509Chain chain)
