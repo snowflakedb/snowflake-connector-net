734d756a8d3cd7d88dcc3db499289d915ece3f31
sfc-gh-ext-simba-lf <louie.farol@insightsoftware.com>
2025-09-25 13:30:43 -0700

    Refactor validator

--- Snowflake.Data/Logger/EasyLoggerValidator.cs
+++ Snowflake.Data/Logger/EasyLoggerValidator.cs
@@ -6,12 +6,19 @@ namespace Snowflake.Data.Logger
 {
     internal class EasyLoggerValidator
     {
-        public static readonly EasyLoggerValidator Instance = new EasyLoggerValidator();
+        private readonly EasyLoggingStarter _easyLoggingStarter;
+
+        public static readonly EasyLoggerValidator Instance = new EasyLoggerValidator(EasyLoggingStarter.Instance);
+
+        public EasyLoggerValidator(EasyLoggingStarter easyLoggingStarter)
+        {
+            _easyLoggingStarter = easyLoggingStarter;
+        }
 
         internal void ValidateLogFilePermissions(UnixStream stream)
         {
             ValidatorOperations.Instance.ValidateUserAndGroupPermissions(stream);
-            if ((stream.FileAccessPermissions & ~EasyLoggingStarter.Instance._logFileUnixPermissions) != 0)
+            if ((stream.FileAccessPermissions & ~_easyLoggingStarter._logFileUnixPermissions) != 0)
                 ValidatorOperations.Instance.ThrowSecurityException("Attempting to read or write to log file with too broad permissions assigned");
         }
     }
