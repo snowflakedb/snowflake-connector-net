58da9b7bbaaa41229d7d82314a20405ddd23dda0
Marcin Gemra <marcin.gemra@snowflake.com>
2025-10-15 11:25:18 +0200

    Add unit tests and fix int test

--- Snowflake.Data.Tests/IntegrationTests/EasyLoggingIT.cs
+++ Snowflake.Data.Tests/IntegrationTests/EasyLoggingIT.cs
@@ -2,6 +2,7 @@ using System;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Mono.Unix;
 using Mono.Unix.Native;
 using NUnit.Framework;
@@ -18,6 +19,7 @@ namespace Snowflake.Data.Tests.IntegrationTests
     public class EasyLoggingIT : SFBaseTest
     {
         private static readonly string s_workingDirectory = Path.Combine(Path.GetTempPath(), $"easy_logging_test_configs_{Path.GetRandomFileName()}");
+        private const string LogDirectoryName = "dotnet";
 
         [OneTimeSetUp]
         public static void BeforeAll()
@@ -39,6 +41,15 @@ namespace Snowflake.Data.Tests.IntegrationTests
         public static void AfterEach()
         {
             EasyLoggingStarter.Instance.Reset(EasyLoggingLogLevel.Off);
+ 
+            var logDirectory = Path.Combine(s_workingDirectory, LogDirectoryName);
+            if (Directory.Exists(logDirectory))
+            {
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    Syscall.chmod(logDirectory, FilePermissions.S_IRUSR | FilePermissions.S_IWUSR | FilePermissions.S_IXUSR);
+                }
+            }
         }
 
         [Test]
@@ -190,8 +201,11 @@ namespace Snowflake.Data.Tests.IntegrationTests
         public void TestFailToEnableEasyLoggingWhenPathIsAccessibleForOthers()
         {
             // arrange
+            var logDirectory = Path.Combine(s_workingDirectory, LogDirectoryName);
+            Directory.CreateDirectory(logDirectory);
+            Syscall.chmod(logDirectory, FilePermissions.S_IRGRP | FilePermissions.S_IWGRP | FilePermissions.S_IXGRP);
+
             var configFilePath = CreateConfigTempFile(s_workingDirectory, Config("WARN", s_workingDirectory, "640"));
-            Syscall.chmod(configFilePath, FilePermissions.S_IRGRP | FilePermissions.S_IWGRP | FilePermissions.S_IXGRP);
             using (IDbConnection conn = new SnowflakeDbConnection())
             {
                 conn.ConnectionString = ConnectionString + $"CLIENT_CONFIG_FILE={configFilePath}";
