b6c9959c931d75caff26972f0453f131281c0822
sfc-gh-ext-simba-lf <louie.farol@insightsoftware.com>
2025-11-05 14:03:38 -0800

    Revert type check

--- Snowflake.Data/Core/Converter/ArrowToStructuredTypeConverter.cs
+++ Snowflake.Data/Core/Converter/ArrowToStructuredTypeConverter.cs
@@ -153,15 +153,13 @@ namespace Snowflake.Data.Core.Converter
                 case Dictionary<string, object> objDict:
                     return CallMethod(targetType, objDict, nameof(ConvertObject));
                 case Dictionary<object, object> mapDict:
-                    if (targetType.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+                    var genericArgs = targetType.GetGenericArguments();
+                    Console.WriteLine(targetType.FullName + $"\n{genericArgs.Length}");
+                    if (genericArgs.Length == 2)
                     {
-                        var genericArgs = targetType.GetGenericArguments();
-                        if (genericArgs.Length >= 2)
-                        {
-                            var keyType = genericArgs[0];
-                            var valueType = genericArgs[1];
-                            return CallMethod(keyType, mapDict, nameof(ConvertMap), valueType);
-                        }
+                        var keyType = genericArgs[0];
+                        var valueType = genericArgs[1];
+                        return CallMethod(keyType, mapDict, nameof(ConvertMap), valueType);
                     }
                     goto default;
                 case List<object> objList:
