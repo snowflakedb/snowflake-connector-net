6bf30713d7dfc6e90965cd89ae357f37d269c87f
Michał Cepiga <michal.cepiga@snowflake.com>
2026-04-30 17:54:50 +0200

    missing jenkins sh script edit

--- ci/container/test_component.sh
+++ ci/container/test_component.sh
@@ -4,8 +4,9 @@ set -o pipefail
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SOURCE_ROOT=${SOURCE_ROOT:-/mnt/host}
 export WORKSPACE=${WORKSPACE:-/mnt/workspace}
+export JENKINS_HOME='/var/jenkins_home'
 
 cd $SOURCE_ROOT
 cp Snowflake.Data.Tests/parameters-local.json Snowflake.Data.Tests/parameters.json
 # todo unignore MaxLobSizeIT after SNOW-1058345 is fixed
-dotnet test -f net6.0 -l "console;verbosity=normal" --logger:"junit;LogFilePath=$WORKSPACE/junit-dotnet.xml" --filter "FullyQualifiedName!=Snowflake.Data.Tests.IntegrationTests.MaxLobSizeIT" 
+dotnet test -f net10.0 -l "console;verbosity=normal" --logger:"junit;LogFilePath=$WORKSPACE/junit-dotnet.xml" --filter "FullyQualifiedName!=Snowflake.Data.Tests.IntegrationTests.MaxLobSizeIT"
