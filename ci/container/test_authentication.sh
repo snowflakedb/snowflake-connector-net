bc5ecb999858e373f9943bd2cf2359154f56aff9
Michał Cepiga <michal.cepiga@snowflake.com>
2026-05-05 22:00:35 +0200

    investigate restore issue

--- ci/container/test_authentication.sh
+++ ci/container/test_authentication.sh
@@ -12,7 +12,7 @@ eval $(jq -r '.authtestparams | to_entries | map("export \(.key)=\(.value|tostri
 export SNOWFLAKE_AUTH_TEST_PRIVATE_KEY_PATH=./.github/workflows/parameters/rsa_keys/rsa_key.p8
 export SNOWFLAKE_AUTH_TEST_INVALID_PRIVATE_KEY_PATH=./.github/workflows/parameters/rsa_keys/rsa_key_invalid.p8
 
-dotnet restore -p:TargetFrameworks=net9.0
+dotnet restore -p:TargetFrameworks=net9.0 --force
 echo "restored"
 dotnet build --no-restore --framework net9.0 -p:TargetFrameworks=net9.0 -v diag
 echo "built"
