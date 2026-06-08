925f2fe4474a4e72a1f1f9eb706772484b70a1ca
Michał Cepiga <michal.cepiga@snowflake.com>
2026-06-08 14:55:36 +0200

    Create test_mac.sh

---
+++ ci/test_mac.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+set -o pipefail
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$(cd "${THIS_DIR}/.." && pwd)"
+
+echo "[INFO] Running Unit Tests for Snowflake .NET Connector"
+
+dotnet test "$ROOT_DIR/Snowflake.Data.Tests/Snowflake.Data.Tests.csproj" \
+    --filter "FullyQualifiedName~Snowflake.Data.Tests.UnitTests" \
+    -l "console;verbosity=normal" \
+    --logger:"junit;LogFilePath=$ROOT_DIR/junit-dotnet-unit.xml"
+
+echo "[INFO] Test Results: $ROOT_DIR/junit-dotnet-unit.xml"
