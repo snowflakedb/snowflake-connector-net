cbdc2af9deb36852a0a619bf6eca1a6cb18689a2
Michał Cepiga <michal.cepiga@snowflake.com>
2026-07-01 09:54:43 +0200

    SNOW-3552882 OpenAsyncResets connection state to Closed on failure.

--- Snowflake.Data/Client/SnowflakeDbConnection.cs
+++ Snowflake.Data/Client/SnowflakeDbConnection.cs
@@ -286,8 +286,8 @@ namespace Snowflake.Data.Client
             }
             try
             {
-                FillConnectionStringFromTomlConfigIfNotSet();
                 OnSessionConnecting();
+                FillConnectionStringFromTomlConfigIfNotSet();
                 var sessionContext = new SessionPropertiesContext
                 {
                     Password = Password,
@@ -324,16 +324,14 @@ namespace Snowflake.Data.Client
                 ConnectionString = _tomlConnectionBuilder.GetConnectionStringFromToml();
         }
 
-        public override Task OpenAsync(CancellationToken cancellationToken)
+        public override async Task OpenAsync(CancellationToken cancellationToken)
         {
             logger.Debug("Open Connection Async.");
             if (_connectionState != ConnectionState.Closed)
             {
                 logger.Debug($"Open with a connection already opened: {_connectionState}");
-                return Task.CompletedTask;
+                return;
             }
-            OnSessionConnecting();
-            FillConnectionStringFromTomlConfigIfNotSet();
             var sessionContext = new SessionPropertiesContext
             {
                 Password = Password,
@@ -342,36 +340,31 @@ namespace Snowflake.Data.Client
                 Token = Token
             };
 
-            return SnowflakeDbConnectionPool
-                .GetSessionAsync(ConnectionString, sessionContext, cancellationToken)
-                .ContinueWith(previousTask =>
-                {
-                    if (previousTask.IsFaulted)
-                    {
-                        // Exception from SfSession.OpenAsync
-                        Exception sfSessionEx = previousTask.Exception;
-                        _connectionState = ConnectionState.Closed;
-                        logger.Error("Unable to connect", sfSessionEx);
-                        throw new SnowflakeDbException(
-                            sfSessionEx,
-                            SnowflakeDbException.CONNECTION_FAILURE_SSTATE,
-                            SFError.INTERNAL_ERROR,
-                            "Unable to connect");
-                    }
-                    else if (previousTask.IsCanceled)
-                    {
-                        _connectionState = ConnectionState.Closed;
-                        logger.Debug("Connection canceled");
-                        throw new TaskCanceledException("Connecting was cancelled");
-                    }
-                    else
-                    {
-                        // Only continue if the session was opened successfully
-                        SfSession = previousTask.Result;
-                        logger.Debug($"Connection open with pooled session: {SfSession.sessionId}");
-                        OnSessionEstablished();
-                    }
-                }, TaskContinuationOptions.None); // this continuation should be executed always (even if the whole operation was canceled) because it sets the proper state of the connection
+            try
+            {
+                OnSessionConnecting();
+                FillConnectionStringFromTomlConfigIfNotSet();
+                var session = await SnowflakeDbConnectionPool.GetSessionAsync(ConnectionString, sessionContext, cancellationToken).ConfigureAwait(false);
+                SfSession = session;
+                logger.Debug($"Connection open with pooled session: {SfSession.sessionId}");
+                OnSessionEstablished();
+            }
+            catch (OperationCanceledException)
+            {
+                _connectionState = ConnectionState.Closed;
+                logger.Debug("Connection canceled");
+                throw new TaskCanceledException("Connecting was cancelled");
+            }
+            catch (Exception ex)
+            {
+                _connectionState = ConnectionState.Closed;
+                logger.Error("Unable to connect", ex);
+
+                if (ex is SnowflakeDbException)
+                    throw;
+
+                throw new SnowflakeDbException(ex, SnowflakeDbException.CONNECTION_FAILURE_SSTATE, SFError.INTERNAL_ERROR, "Unable to connect");
+            }
         }
 
         public Mutex GetArrayBindingMutex()
