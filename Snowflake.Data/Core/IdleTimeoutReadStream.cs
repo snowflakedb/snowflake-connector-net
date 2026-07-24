ec1d03c772425549beadd7abf9157e0cf6e56f30
Michał Cepiga <michal.cepiga@snowflake.com>
2026-07-24 16:43:02 +0200

    improvement

--- Snowflake.Data/Core/IdleTimeoutReadStream.cs
+++ Snowflake.Data/Core/IdleTimeoutReadStream.cs
@@ -10,7 +10,7 @@ internal sealed class IdleTimeoutReadStream : Stream
     private readonly Stream _inner;
     private readonly TimeSpan _idleTimeout;
     private readonly TimeSpan _readTimeout;
-    private readonly Lazy<Timer> _idleTimer;
+    private readonly Timer _idleTimer;
     private volatile bool _idleExpired;
 
     public IdleTimeoutReadStream(Stream inner, TimeSpan idleTimeout, TimeSpan readTimeout)
@@ -18,14 +18,43 @@ internal sealed class IdleTimeoutReadStream : Stream
         _inner = inner;
         _idleTimeout = idleTimeout;
         _readTimeout = readTimeout;
-        _idleTimer = new Lazy<Timer>(() => new Timer(_ => _idleExpired = true, null, idleTimeout.Ticks == 0 ? Timeout.InfiniteTimeSpan : _idleTimeout, Timeout.InfiniteTimeSpan));
+        _idleTimer = new(_ => _idleExpired = true, null, idleTimeout.Ticks == 0 ? Timeout.InfiniteTimeSpan : _idleTimeout, Timeout.InfiniteTimeSpan);
     }
 
-    public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
     {
         if (_idleTimeout.Ticks <= 0 && _readTimeout.Ticks <= 0)
-            return await _inner.ReadAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
+            return _inner.ReadAsync(buffer, offset, count, cancellationToken);
 
+        return ReadWithTimeoutsAsync(buffer, offset, count, cancellationToken);
+    }
+
+    public override int Read(byte[] buffer, int offset, int count) =>
+        _inner.Read(buffer, offset, count);
+
+    public override bool CanRead => _inner.CanRead;
+    public override bool CanSeek => _inner.CanSeek;
+    public override bool CanWrite => false;
+    public override long Length => _inner.Length;
+    public override long Position { get => _inner.Position; set => _inner.Position = value; }
+    public override void Flush() => _inner.Flush();
+    public override long Seek(long offset, SeekOrigin origin) => _inner.Seek(offset, origin);
+    public override void SetLength(long value) => _inner.SetLength(value);
+    public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException("This decorator has no writing capabilities!");
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _idleTimer.Dispose();
+            _inner.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    private async Task<int> ReadWithTimeoutsAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    {
         if (_idleExpired)
             throw new TimeoutException($"No data received for {_idleTimeout.TotalSeconds} seconds");
 
@@ -59,32 +88,6 @@ internal sealed class IdleTimeoutReadStream : Stream
         if (_idleTimeout.Ticks <= 0)
             return;
 
-        _idleTimer.Value.Change(_idleTimeout, Timeout.InfiniteTimeSpan);
-    }
-
-    public override int Read(byte[] buffer, int offset, int count) =>
-        _inner.Read(buffer, offset, count);
-
-    public override bool CanRead => _inner.CanRead;
-    public override bool CanSeek => _inner.CanSeek;
-    public override bool CanWrite => false;
-    public override long Length => _inner.Length;
-    public override long Position { get => _inner.Position; set => _inner.Position = value; }
-    public override void Flush() => _inner.Flush();
-    public override long Seek(long offset, SeekOrigin origin) => _inner.Seek(offset, origin);
-    public override void SetLength(long value) => _inner.SetLength(value);
-    public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException("This decorator has no writing capabilities!");
-
-    protected override void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            if (_idleTimer.IsValueCreated)
-                _idleTimer.Value.Dispose();
-
-            _inner.Dispose();
-        }
-
-        base.Dispose(disposing);
+        _idleTimer.Change(_idleTimeout, Timeout.InfiniteTimeSpan);
     }
 }
