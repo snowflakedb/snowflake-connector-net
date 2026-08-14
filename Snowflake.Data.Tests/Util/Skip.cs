767928c58ec0b56b3abf8d590cedc6632aa372b6
Michał Cepiga <michal.cepiga@snowflake.com>
2026-08-14 10:53:23 +0200

    NO-SNOW fix flaky wiremock test (#1426)

--- Snowflake.Data.Tests/Util/Skip.cs
+++ Snowflake.Data.Tests/Util/Skip.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit.Sdk;
 
 namespace Snowflake.Data.Tests.Util;
@@ -8,5 +9,44 @@ public static class Skip
     {
         if (condition)
             throw SkipException.ForSkip(rationale);
+    }
+
+    public static void WhenOnTfm(Tfm tfm, string rationale)
+    {
+#if NETFRAMEWORK
+        var skip = tfm.HasFlag(Tfm.Framework);
+        When(skip, rationale);
+#endif
+#if NET6_0
+        var skip = tfm.HasFlag(Tfm.Net6);
+        When(skip, rationale);
+#endif
+#if NET7_0
+        var skip = tfm.HasFlag(Tfm.Net7);
+        When(skip, rationale);
+#endif
+#if NET8_0
+        var skip = tfm.HasFlag(Tfm.Net8);
+        When(skip, rationale);
+#endif
+#if NET9_0
+        var skip = tfm.HasFlag(Tfm.Net9);
+        When(skip, rationale);
+#endif
+#if NET10_0
+        var skip = tfm.HasFlag(Tfm.Net10);
+        When(skip, rationale);
+#endif
+    }
+
+    [Flags]
+    public enum Tfm
+    {
+        Framework,
+        Net6,
+        Net7,
+        Net8,
+        Net9,
+        Net10
     }
 }
